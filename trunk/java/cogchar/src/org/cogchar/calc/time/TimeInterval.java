a089066b487a46b43dd878eec5f36a5cedf6c3e0
bulby <bulby@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2011-04-14 01:26:48 +0000

    Added copyright notices.

--- trunk/java/cogchar/src/org/cogchar/calc/time/TimeInterval.java
+++ trunk/java/cogchar/src/org/cogchar/calc/time/TimeInterval.java
@@ -1,6 +1,17 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ *  Copyright 2011 by The Cogchar Project (www.cogchar.org).
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
  */
 
 package org.cogchar.calc.time;
@@ -16,23 +27,23 @@ import org.jscience.mathematics.number.FieldNumber;
  * Duration must be positive <--->   End point must be after start point.
  */
 public class TimeInterval<SecondsFN extends FieldNumber<SecondsFN>> {
-	private		TimePoint<SecondsFN>		myStartPoint;
-	private		TimePoint<SecondsFN>		myEndPoint;
+	private		ExactTimePoint<SecondsFN>		myStartPoint;
+	private		ExactTimePoint<SecondsFN>		myEndPoint;
 	private		SecondsFN					myDurationSec;
 
-	public TimeInterval(TimePoint<SecondsFN> start, TimePoint<SecondsFN> end) {
+	public TimeInterval(ExactTimePoint<SecondsFN> start, ExactTimePoint<SecondsFN> end) {
 		myStartPoint = start;
 		myEndPoint = end;
 	}
-	public TimeInterval(TimePoint<SecondsFN> start, SecondsFN duration) {
+	public TimeInterval(ExactTimePoint<SecondsFN> start, SecondsFN duration) {
 		myStartPoint = start;
 		myDurationSec = duration;
 	}
-	public TimeInterval(SecondsFN duration, TimePoint<SecondsFN> end) {
+	public TimeInterval(SecondsFN duration, ExactTimePoint<SecondsFN> end) {
 		myDurationSec = duration;
 		myEndPoint = end;
 	}
-	public TimePoint<SecondsFN> getStartPoint() {
+	public ExactTimePoint<SecondsFN> getStartPoint() {
 		if (myStartPoint == null) {
 			
 		}
