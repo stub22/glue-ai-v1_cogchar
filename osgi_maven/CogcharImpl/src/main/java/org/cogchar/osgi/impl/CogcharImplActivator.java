55eb51dfcfd5db5af244221e612a7e6fe9d66379
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2011-07-17 04:39:57 +0000

    OSGi module containing simplified WomanFaceTest and some JMonkey tests works from Netbeans 7.0, with various caveats.

--- osgi_maven/CogcharImpl/src/main/java/org/cogchar/osgi/impl/CogcharImplActivator.java
+++ osgi_maven/CogcharImpl/src/main/java/org/cogchar/osgi/impl/CogcharImplActivator.java
@@ -5,6 +5,8 @@
 
 package org.cogchar.osgi.impl;
 
+import org.cogchar.bony.TestChooserWrapper;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -14,13 +16,31 @@ import org.osgi.framework.BundleContext;
  */
 public class CogcharImplActivator implements BundleActivator {
 
-	@Override
-    public void start(BundleContext context) throws Exception {
-        System.out.println("CogcharImplActivator.start()");
+	@Override public void start(BundleContext context) throws Exception {
+        System.out.println("CogcharImplActivator.start(), bundleContext=" + context);
+		Bundle b = context.getBundle();
+		System.out.println("bundle=" + b);
+		
+
+		ClassLoader tccl = Thread.currentThread().getContextClassLoader();
+		System.out.println("Saved old class loader: " + tccl);
+		try {
+			ClassLoader localLoader = getClass().getClassLoader();
+			System.out.println("Setting thread loader to local loader: " + localLoader);
+			Thread.currentThread().setContextClassLoader(localLoader);
+			// WomanFaceTest.main(null);
+			TestChooserWrapper.displayTestChooser(b, null);
+			// jme3test.helloworld.HelloAnimation.main(null);
+			// jme3test.TestChooser.main(null);
+		} finally {
+			System.out.println("Restoring old loader: " + tccl);
+			Thread.currentThread().setContextClassLoader(tccl);
+		}
+		// System.out.println("Returned from WomanFaceTest.main()");
+
     }
 
-	@Override
-    public void stop(BundleContext context) throws Exception {
+	@Override public void stop(BundleContext context) throws Exception {
         System.out.println("CogcharImplActivator.stop()");
     }
 
