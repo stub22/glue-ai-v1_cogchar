cd4e996f96e122c14cfeae4fa7257d3e2a54a9f7
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2011-07-24 22:19:45 +0000

    New classes from bony refactor.

---
+++ src_java/org/cogchar/bony/StickFigureTestMain.java
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2011 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cogchar.bony;
+
+import javax.swing.JFrame;
+
+/**
+ * @author Stu B. <www.texpedient.com>
+ */
+public class StickFigureTestMain {
+	static String	sceneFilePath = "leo_hanson_tests/test3/test3.scene";
+	static float	sceneLocalScale = 0.5f;
+	static int		canvasWidth = 640, canvasHeight = 480;
+	
+	public static void main(String[] args) {
+		BonyStickFigureApp stickFigureApp = new BonyStickFigureApp(sceneFilePath, sceneLocalScale);
+		BonySystem.setJMonkeySettings(stickFigureApp, canvasWidth, canvasHeight);
+		VirtCharPanel vcp = stickFigureApp.makeCharPanelWithCanvas();
+		JFrame jf = vcp.makeEnclosingJFrame();
+		stickFigureApp.startJMonkeyCanvas();
+		stickFigureApp.setScoringFlag(true);		
+		// owTst.start(JmeContext.Type.Display);
+	}	
+}
