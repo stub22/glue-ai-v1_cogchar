d2ac0f1b46c03510de24ae7e1ab82536768d5e4f
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2011-10-08 02:04:00 +0000

    Improved window-close and bundle.stop() behavior.

--- src_java_render/org/cogchar/render/opengl/bony/VirtCharPanel.java
+++ src_java_render/org/cogchar/render/opengl/bony/VirtCharPanel.java
@@ -59,8 +59,18 @@ public class VirtCharPanel extends javax.swing.JPanel {
 	public JFrame makeEnclosingJFrame() {
 		theLogger.info("Making frame");
 		JFrame frame = new JFrame(this.getClass().getName());
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		/*
+		 * public void setDefaultCloseOperation(int operation)
+Sets the operation that will happen by default when the user initiates a "close" on this frame. You must specify one of the following choices:
+DO_NOTHING_ON_CLOSE (defined in WindowConstants): Don't do anything; require the program to handle the operation in the windowClosing method of a registered WindowListener object.
+HIDE_ON_CLOSE (defined in WindowConstants): Automatically hide the frame after invoking any registered WindowListener objects.
+DISPOSE_ON_CLOSE (defined in WindowConstants): Automatically hide and dispose the frame after invoking any registered WindowListener objects.
+EXIT_ON_CLOSE (defined in JFrame): Exit the application using the System exit method. Use this only in applications.
+The value is set to HIDE_ON_CLOSE by default. Changes to the value of this property cause the firing of a property change event, with property name "defaultCloseOperation".
+		 */
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().add(this, BorderLayout.CENTER);
+
 		theLogger.info("Packing frame");
 		frame.pack();
 		theLogger.info("Setting frame visible");
