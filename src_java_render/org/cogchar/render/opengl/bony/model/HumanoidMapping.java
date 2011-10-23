335bc8edb9090d47b8da62f043c006a40147c095
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2011-10-23 17:44:42 +0000

    pulling in features that were prototyped under demo project.

---
+++ src_java_render/org/cogchar/render/opengl/bony/model/HumanoidMapping.java
@@ -0,0 +1,53 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.cogchar.render.opengl.bony.model;
+
+import com.jme3.animation.AnimControl;
+import com.jme3.asset.AssetManager;
+import com.jme3.bullet.control.KinematicRagdollControl;
+import com.jme3.material.Material;
+import com.jme3.math.ColorRGBA;
+import com.jme3.math.FastMath;
+import com.jme3.scene.Node;
+import com.jme3.scene.debug.SkeletonDebugger;
+
+/**
+ * @author Stu B. <www.texpedient.com>
+ */
+public class HumanoidMapping {
+
+    public static void addHumanoidBonesToRagdoll(KinematicRagdollControl krc) {
+        krc.addBoneName("Ulna.L");
+        krc.addBoneName("Ulna.R");
+        krc.addBoneName("Chest");
+        krc.addBoneName("Foot.L");
+        krc.addBoneName("Foot.R");
+        krc.addBoneName("Hand.R");
+        krc.addBoneName("Hand.L");
+        krc.addBoneName("Neck");
+        krc.addBoneName("Root");
+        krc.addBoneName("Stomach");
+        krc.addBoneName("Waist");
+        krc.addBoneName("Humerus.L");
+        krc.addBoneName("Humerus.R");
+        krc.addBoneName("Thigh.L");
+        krc.addBoneName("Thigh.R");
+        krc.addBoneName("Calf.L");
+        krc.addBoneName("Calf.R");
+        krc.addBoneName("Clavicle.L");
+        krc.addBoneName("Clavicle.R");
+
+    }
+	public static void applyHumanoidJointLimits(KinematicRagdollControl krc) { 
+		float eighth_pi = FastMath.PI * 0.125f;
+        krc.setJointLimit("Waist", eighth_pi, eighth_pi, eighth_pi, eighth_pi, eighth_pi, eighth_pi);
+        krc.setJointLimit("Chest", eighth_pi, eighth_pi, 0, 0, eighth_pi, eighth_pi);
+		//JMonkey original commented out line and comment...
+        //  Oto's head is almost rigid
+        //    myHumanoidKRC.setJointLimit("head", 0, 0, eighth_pi, -eighth_pi, 0, 0);
+	}
+	
+	
+}
