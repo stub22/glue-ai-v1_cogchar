8bbd99eef5d2b0cf9d76d039c953be8dcd41d0e7
matt.r.stevenson <matt.r.stevenson@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-04-18 23:22:16 +0000

    Temporarily disabling cogbot.

--- maven/org.cogchar.bundle.bind.cogbot/src/main/java/org/cogchar/bind/cogbot/osgi/Activator.java
+++ maven/org.cogchar.bundle.bind.cogbot/src/main/java/org/cogchar/bind/cogbot/osgi/Activator.java
@@ -1,6 +1,5 @@
 package org.cogchar.bind.cogbot.osgi;
 
-import org.cogchar.bind.cogbot.main.CogbotSpeechDemo;
 import org.osgi.framework.BundleActivator;
 import org.osgi.framework.BundleContext;
 
@@ -10,7 +9,7 @@ public class Activator implements BundleActivator {
 		System.out.println("******************************************************************************");
 		System.out.println("Cogbot binding Activator says Hi!");
 		System.out.println("******************************************************************************");
-        CogbotSpeechDemo.main(new String[]{"192.168.0.100"});
+        //CogbotSpeechDemo.main(new String[]{"192.168.0.100"});
     }
 
     public void stop(BundleContext context) throws Exception {
