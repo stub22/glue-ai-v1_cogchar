e94ba16e20ef4dd9eec995e4f554d950a76427e2
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2012-05-13 16:49:41 +0000

    Moved JME3 init control to PUMA, fixed problems with bundle-security under Netigso.

--- maven/org.cogchar.bundle.render.opengl/src/main/java/org/cogchar/render/opengl/osgi/RenderBundleActivator.java
+++ maven/org.cogchar.bundle.render.opengl/src/main/java/org/cogchar/render/opengl/osgi/RenderBundleActivator.java
@@ -15,58 +15,32 @@
  */
 package org.cogchar.render.opengl.osgi;
 
-import java.awt.event.WindowEvent;
-import javax.swing.JFrame;
-import org.appdapter.core.log.BasicDebugger;
 import org.appdapter.osgi.core.BundleActivatorBase;
-import org.cogchar.blob.emit.BonyConfigEmitter;
-import org.cogchar.render.app.bony.BonyVirtualCharApp;
-import org.cogchar.render.app.bony.BonyRenderContext;
-import org.osgi.framework.BundleContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
-import org.cogchar.render.app.humanoid.HumanoidPuppetApp;
-import org.cogchar.render.gui.bony.PanelUtils;
-import org.cogchar.render.gui.bony.VirtualCharacterPanel;
+import org.osgi.framework.BundleContext;
+
 
 /**
  * @author Stu B. <www.texpedient.com>
  */
 public class RenderBundleActivator extends BundleActivatorBase {
-	
 
-	static Logger theLogger = LoggerFactory.getLogger(RenderBundleActivator.class);
-	// private BonyRenderContext myBonyRenderContext;
-
-	@Override protected Logger getLogger() {
-		return theLogger;
-	}
-
-	// This is the primary service export point for the bundle, as of 2011-08-30.
-	/*
-	public BonyRenderContext getBonyRenderContext() {
-		return myBonyRenderContext;
-	}
-	* 
-	*/ 
 
 	@Override public void start(BundleContext bundleCtx) throws Exception {
 		
 		super.start(bundleCtx);
 		
-		String panelKind = "SLIM";
-		
-		// myBonyRenderContext = 
-		RenderBundleUtils.buildBonyRenderContextInOSGi(bundleCtx, panelKind);
+		// Now done later by PUMA or other.
+		// String panelKind = "SLIM";
+		// RenderBundleUtils.buildBonyRenderContextInOSGi(bundleCtx, panelKind);
 	
-		theLogger.info("******************* start() is DONE!");
+		logInfo("******************* start() is DONE!");
 	}
 
 	@Override public void stop(BundleContext bundleCtx) throws Exception {
 		// Perhaps this should be done last, via a "finally" clause.
 		super.stop(bundleCtx);
 		RenderBundleUtils.shutdownBonyRenderContextInOSGi(bundleCtx);
-		theLogger.info("stop() is DONE!");
+		logInfo("stop() is DONE!");
 	}
 }
