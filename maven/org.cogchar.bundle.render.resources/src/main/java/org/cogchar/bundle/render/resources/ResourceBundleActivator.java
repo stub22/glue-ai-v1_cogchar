6fb2c2fc506fa526510736a00418e302bb33b06a
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-05-23 00:00:38 +0000

    Reduced logging,removed some unused code.

--- maven/org.cogchar.bundle.render.resources/src/main/java/org/cogchar/bundle/render/resources/ResourceBundleActivator.java
+++ maven/org.cogchar.bundle.render.resources/src/main/java/org/cogchar/bundle/render/resources/ResourceBundleActivator.java
@@ -2,39 +2,22 @@ package org.cogchar.bundle.render.resources;
 
 import org.appdapter.osgi.core.BundleActivatorBase;
 import org.osgi.framework.BundleContext;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import org.cogchar.render.sys.core.AssetContext;
 import org.cogchar.render.sys.core.JmonkeyAssetLocation;
 import org.cogchar.render.sys.core.RenderRegistryFuncs;
 
 public class ResourceBundleActivator extends BundleActivatorBase {
-	static Logger theLogger = LoggerFactory.getLogger(ResourceBundleActivator.class);
 
-	//public static URL	theBundleRootURL;
-	//public static ResourceBundleActivator theInstance;
-	@Override protected Logger getLogger() {
-		return theLogger;
-	}	
+
 	@Override public void start(BundleContext bundleCtx) throws Exception {
 		super.start(bundleCtx);
-		theLogger.info("******************* Registering assumed resource bundle with default AssetContext");
-		//
+		logInfo("******************* Registering assumed resource bundle with default AssetContext");
+		
 		AssetContext defAssetCtx = RenderRegistryFuncs.findOrMakeAssetContext(null, null, ResourceBundleActivator.class);
 		JmonkeyAssetLocation jmal = new JmonkeyAssetLocation(ResourceBundleActivator.class);
 		defAssetCtx.addAssetSource(jmal);		
-		/*
-		Bundle bun = bundleCtx.getBundle();
-		theLogger.info("Got bundle: " + bun);
-		theLogger.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Bundle location: " + bun.getLocation());
-		theLogger.info("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% Bundle symbolic name: " + bun.getSymbolicName());
-		URL bundleRootURL = bun.getEntry("/");
-		theLogger.info("Root URL: " + bundleRootURL);
-		theBundleRootURL = bundleRootURL;
-		theInstance = this;
-		 * 
-		 */
+
 	}
 
 }
