9d10a4d5e75eab753933a3b2332a99c4d401769c
matt.r.stevenson <1117794+matt-stevenson@users.noreply.github.com>
2012-05-24 20:05:23 +0000

    Using JFlux API. Added convo and dictation demo projects.

---
+++ maven/org.cogchar.bundle.demo.dictation/src/main/java/org/cogchar/bundle/demo/dictation/ui/DefaultDictationGrabber.java
@@ -0,0 +1,206 @@
+/*
+ * Copyright 2012 by The Cogchar Project (www.cogchar.org).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.cogchar.bundle.demo.dictation.ui;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.jms.Connection;
+import javax.jms.Destination;
+import javax.jms.JMSException;
+import javax.jms.Session;
+import org.cogchar.bundle.demo.dictation.sound.SoundDetector;
+import org.jflux.api.core.node.ConsumerNode;
+import org.jflux.api.core.node.chain.NodeChainBuilder;
+import org.jflux.api.core.util.Adapter;
+import org.jflux.impl.messaging.JMSAvroUtils;
+import org.jflux.impl.messaging.jms.MessageHeaderAdapter;
+import org.robokind.api.common.utils.TimeUtils;
+import org.robokind.api.speechrec.SpeechRecEvent;
+import org.robokind.api.speechrec.SpeechRecEventList;
+import org.robokind.avrogen.speechrec.SpeechRecEventListRecord;
+import org.robokind.impl.messaging.utils.ConnectionManager;
+import org.robokind.impl.speechrec.PortableSpeechRecEvent;
+import org.robokind.impl.speechrec.PortableSpeechRecEventList;
+
+/**
+ *
+ * @author Matthew Stevenson <www.robokind.org>
+ */
+public class DefaultDictationGrabber implements DictationGrabber{
+    private final static Logger theLogger = Logger.getLogger(DefaultDictationGrabber.class.getName());
+    private static DateFormat theDateFormat = DateFormat.getDateTimeInstance(DateFormat.SHORT, DateFormat.MEDIUM);
+    private DictationPanel myPanel;
+    private SoundDetector mySoundDetector;
+    private ConsumerNode<String> mySpeechRecSender;
+    private Connection myConnection;
+    private Session mySession;
+    private String myCapturedDictation;
+    
+    public DefaultDictationGrabber(DictationPanel dp){
+        myPanel = dp;
+        mySoundDetector = new SoundDetector(this);
+        myCapturedDictation = "";
+    }
+
+    @Override
+    public synchronized void handleDictation(){
+        collectDictation();
+        if(mySpeechRecSender == null){
+            myCapturedDictation = "";
+            myPanel.txtConcatInput.setText("");
+            return;
+        }else if(myCapturedDictation == null || myCapturedDictation.isEmpty()){
+            return;
+        }
+        mySpeechRecSender.getListener().handleEvent(myCapturedDictation);
+        myPanel.logSpeech(formatSpeech(myCapturedDictation));
+        myCapturedDictation = "";
+        myPanel.txtConcatInput.setText("");
+    }
+    
+    private String formatSpeech(String str){
+        str = "[" + theDateFormat.format(new Date()) + "]: " + str;
+        String s = myPanel.txtMatches.getText();
+        if(s != null && !s.isEmpty()){
+            str = "\n\n" + str;
+        }
+        return str;
+    }    
+
+    @Override
+    public synchronized boolean collectDictation() {
+        String input = myPanel.txtConcatInput.getText();
+        if(input == null || input.isEmpty() 
+                || input.equals(myCapturedDictation)){
+            return false;
+        }
+        myCapturedDictation = input;
+        return true;
+    }
+    
+    public boolean connect(String address, String dest){
+        if(myConnection != null || mySession != null){
+            return false;
+        }
+        try{
+            mySpeechRecSender = buildSpeechSenderNodeChain(address, dest);
+            if(mySpeechRecSender == null){
+                return false;
+            }
+            return mySpeechRecSender.start();
+        }catch(JMSException ex){
+            theLogger.log(Level.WARNING, "Error building speech sender.", ex);
+            return false;
+        }
+    }
+    
+    public void disconnect(){
+        setAutoSend(false);
+        if(mySpeechRecSender != null){
+            mySpeechRecSender.stop();
+            mySpeechRecSender = null;
+        }
+        if(mySession != null){
+            try{
+                mySession.close();
+            }catch(JMSException ex){}
+            mySession = null;
+        }
+        if(myConnection != null){
+            try{
+                myConnection.close();
+            }catch(JMSException ex){}
+            myConnection = null;
+        }        
+    }
+    
+    public boolean setAutoSend(boolean value){
+        if(mySpeechRecSender == null){
+            mySoundDetector.stop();
+            return false;
+        }
+        if(value){
+            return mySoundDetector.start();
+        }else{
+            mySoundDetector.stop();
+        }
+        return value;
+    }
+    
+    public SoundDetector getSoundDetector() {
+        return mySoundDetector;
+    }
+    
+    private ConsumerNode<String> buildSpeechSenderNodeChain(
+            String ip, String destStr) throws JMSException{
+        myConnection = ConnectionManager.createConnection(
+                "admin", "admin", "client1", "test", "tcp://" + ip + ":5672");
+        if(myConnection == null){
+            return null;
+        }
+        myConnection.start();
+        mySession = 
+                myConnection.createSession(false, Session.CLIENT_ACKNOWLEDGE);
+        if(mySession == null){
+            return null;
+        }
+        Destination dest = ConnectionManager.createDestination(destStr);
+        if(dest == null){
+            return null;
+        }
+        ConsumerNode<SpeechRecEventList> ttsNode = 
+                JMSAvroUtils.buildEventSenderChain(
+                        SpeechRecEventList.class, 
+                        SpeechRecEventListRecord.class, 
+                        SpeechRecEventListRecord.SCHEMA$, 
+                        new PortableSpeechRecEventList.MessageRecordAdapter(),
+                        mySession, dest, 
+                        new MessageHeaderAdapter("application/speechRecEventList"));
+        return NodeChainBuilder.build(String.class)
+                .attach(SpeechRecEventList.class, 
+                        new SpeechRecEventListFormatter("source", "dest"))
+                .getConsumerChain(ttsNode);
+    }
+    
+    public static class SpeechRecEventListFormatter implements 
+            Adapter<String, SpeechRecEventList> {
+        private String mySourceId;
+        private String myDestId;
+
+        public SpeechRecEventListFormatter(String sourceId, String destId){
+            if(sourceId == null || destId == null){
+                throw new NullPointerException();
+            }
+            mySourceId = sourceId;
+            myDestId = destId;
+        }
+
+        @Override
+        public SpeechRecEventList adapt(String a) {
+            SpeechRecEvent event = new PortableSpeechRecEvent(
+                    mySourceId, a, 1.0, TimeUtils.now());
+            List<SpeechRecEvent> list = new ArrayList<SpeechRecEvent>(1);
+            list.add(event);
+            return new PortableSpeechRecEventList(
+                    mySourceId, myDestId, list, TimeUtils.now());
+        }
+
+    }
+}
