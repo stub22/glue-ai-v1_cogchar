84c3c2eca983005c65eae5b6fe54a63b79498af2
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-05-29 23:37:09 +0000

    Moved older conversation code into separate lib.convoid project.

---
+++ trunk/src_scala_core/org/cogchar/blob/emit/ZenoConfigEmitter.scala
@@ -0,0 +1,38 @@
+/*
+ *  Copyright 2012 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cogchar.blob.emit
+
+/**
+ * @author Stu B. <www.texpedient.com>
+ */
+
+class ZenoConfigEmitter extends BonyConfigEmitter {
+	// Commercial "Extra Robot" model, not loadable without a license, ignored in those cases.
+/*
+	val EXTRA_ROBOT_MESH_PATH = "zenobot_06_20120323/zenobot_06_20120323.mesh.xml";		
+	
+	// val EXTRA_ROBOT_MESH_PATH = "zenobot_05_20120309/ZenoBot05.ma.mesh.xml";		
+	//		String dualCharURI = "urn:org.cogchar/platform/nb701?char=HRK_Zeno_R50&version=20120302";   
+	//		
+	//			
+	val HRK_URN_PREFIX = "urn:fdt:com.hrk/";
+	val ZENO_R50_NICKNAME = "ZenoR50";
+	val	ZENO_R50_CHAR_URI = COGCHAR_CHAR_URN_PREFIX + ZENO_R50_NICKNAME;
+	val	ZENO_R50_CHAR_IDENT = new FreeIdent(ZENO_R50_CHAR_URI, ZENO_R50_NICKNAME)
+*/
+
+}
