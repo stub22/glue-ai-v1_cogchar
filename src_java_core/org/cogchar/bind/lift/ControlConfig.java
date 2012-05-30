b96b02e10849804d371c59995363892b42426cbd
RyanBiggs <RyanBiggs@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-05-30 23:42:24 +0000

    Now additions to webapp functionality will not require changes to Cog Char. Hurray! (Cog Char component)

--- src_java_core/org/cogchar/bind/lift/ControlConfig.java
+++ src_java_core/org/cogchar/bind/lift/ControlConfig.java
@@ -27,7 +27,8 @@ public class ControlConfig {
 
 	static Logger theLogger = LoggerFactory.getLogger(ControlConfig.class);
 	public String myURI_Fragment;
-	public ControlType controlType;
+	//public ControlType controlType; // Probably a good idea to do away with this enum type - see comments at bottom
+	public String controlType;
 	public int id;
 	public String action;
 	public String text;
@@ -36,12 +37,13 @@ public class ControlConfig {
 
 	@Override
 	public String toString() {
-		return "ControlConfig[uriFrag=" + myURI_Fragment + ", Type=" + controlType.name() + ", id=" + id + ", text=\""
+		return "ControlConfig[uriFrag=" + myURI_Fragment + ", Type=" + controlType + ", id=" + id + ", text=\""
 				+ text + "\", style=" + style + " resource=" + resource + "]";
 	}
 
 	public ControlConfig(Item configItem) {
 		myURI_Fragment = configItem.getIdent().getLocalName();
+		/* This is for using ControlType enum type, which we are probably phasing out
 		String typeString = ItemFuncs.getString(configItem, LiftConfigNames.P_controlType, null);
 		controlType = ControlType.NULLTYPE;
 		for (ControlType testType : ControlType.values()) {
@@ -52,6 +54,8 @@ public class ControlConfig {
 		if (controlType == ControlType.NULLTYPE) {
 			theLogger.warn("Lift Control with URI Fragment " + myURI_Fragment + " does not indicate a valid type!");
 		}
+		*/
+		controlType = ItemFuncs.getString(configItem, LiftConfigNames.P_controlType, "NULLTYPE");
 		id = ItemFuncs.getInteger(configItem, LiftConfigNames.P_controlId, 0);
 		action = ItemFuncs.getString(configItem, LiftConfigNames.P_controlAction, "");
 		text = ItemFuncs.getString(configItem, LiftConfigNames.P_controlText, "");
@@ -59,8 +63,11 @@ public class ControlConfig {
 		resource = ItemFuncs.getString(configItem, LiftConfigNames.P_controlResource, "");
 	}
 
+	/* Let's try doing away with this. It makes sense to have enum type for jME stuff internal to Cog Char, but 
+	 * here it requires changes to Cog Char every time we add web app functionality
 	public enum ControlType {
 
 		NULLTYPE, PUSHYBUTTON, TEXTINPUT, SELECTBOXES, RADIOBUTTONS, LISTBOX
 	}
+	*/
 }
