e7c50a980f45d38bbc478a010d64789ae9229856
RyanBiggs <RyanBiggs@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-06-14 09:42:05 +0000

    For consistency, light and camera names now derived from RDF URIs instead of separate attribute

--- trunk/src_java_core/org/cogchar/api/scene/LightConfig.java
+++ trunk/src_java_core/org/cogchar/api/scene/LightConfig.java
@@ -15,41 +15,44 @@
  */
 package org.cogchar.api.scene;
 
-
 import java.util.Arrays;
 import org.appdapter.core.item.Item;
 import org.appdapter.core.item.ItemFuncs;
-
 
 /**
  * @author Ryan Biggs
  */
 public class LightConfig {
-	public String						myURI_Fragment;
-	public String                       lightName;
-	public LightType                    lightType;
-	public float[]						lightDirection = new float[3];
-	public float[]						lightColor = new float[4];
-           
+
+	public String myURI_Fragment;
+	public String lightName;
+	public LightType lightType;
+	public float[] lightDirection = new float[3];
+	public float[] lightColor = new float[4];
+
 	@Override
 	public String toString() {
-		return "LightConfig[uriFrag=" + myURI_Fragment + ", type=" + lightType.name() + /* ", pos=" + Arrays.toString(cameraPosition) + */ ", dir=" + Arrays.toString(lightDirection) + ", color=" + Arrays.toString(lightColor) + "]";
+		return "LightConfig[name=" + lightName + ", type=" + lightType.name()
+				+ ", dir=" + Arrays.toString(lightDirection) + ", color=" + Arrays.toString(lightColor) + "]";
 	}
 
 	public LightConfig(Item configItem) {
-		myURI_Fragment = configItem.getIdent().getLocalName();
-		lightName = ItemFuncs.getString(configItem, SceneConfigNames.P_lightName, null);
+		lightName = configItem.getIdent().getLocalName();
 		lightType = LightType.AMBIENT; // For now, we assume light is ambient (no direction required) if type is not specified
 		String typeString = ItemFuncs.getString(configItem, SceneConfigNames.P_lightType, null);
-		if (typeString.equals("DIRECTIONAL")) {lightType = LightType.DIRECTIONAL;}
-		for (int index=0; index<lightDirection.length; index++) {
+		if (typeString.equals("DIRECTIONAL")) {
+			lightType = LightType.DIRECTIONAL;
+		}
+		for (int index = 0; index < lightDirection.length; index++) {
 			lightDirection[index] = ItemFuncs.getDouble(configItem, SceneConfigNames.P_direction[index], 0.0).floatValue();
 		}
-		for (int index=0; index<lightColor.length; index++) {
+		for (int index = 0; index < lightColor.length; index++) {
 			lightColor[index] = ItemFuncs.getDouble(configItem, SceneConfigNames.P_color[index], null).floatValue();
 		}
 	}
-        
-	public enum LightType {DIRECTIONAL, AMBIENT} // We can add POINT and SPOT later if we want, will need to extend the RDF definition to do so
 
+	public enum LightType {
+
+		DIRECTIONAL, AMBIENT
+	} // We can add POINT and SPOT later if we want, will need to extend the RDF definition to do so
 }
