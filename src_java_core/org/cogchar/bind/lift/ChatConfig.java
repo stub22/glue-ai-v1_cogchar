a268d541857e5838fb3f2654a68d5b38d6bfd77f
Ryan Biggs <ryan@robokindrobots.com>
2012-06-24 05:58:35 +0000

    Chat app configuration including Cogbot conversation URL is now loaded from RDF (com.hrkind.content.preview metadata/chatbird/cogbotZenoAmazonEC.ttl)

---
+++ src_java_core/org/cogchar/bind/lift/ChatConfig.java
@@ -0,0 +1,78 @@
+/*
+ *  Copyright 2012 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.cogchar.bind.lift;
+
+import com.hp.hpl.jena.assembler.Assembler;
+import com.hp.hpl.jena.assembler.Mode;
+import com.hp.hpl.jena.rdf.model.Resource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import org.appdapter.bind.rdf.jena.assembly.AssemblerUtils;
+import org.appdapter.bind.rdf.jena.assembly.DynamicCachingComponentAssembler;
+import org.appdapter.core.component.KnownComponentImpl;
+import org.appdapter.core.item.Item;
+import org.appdapter.core.item.ItemFuncs;
+import org.appdapter.core.log.BasicDebugger;
+
+/**
+ * Used to enclose data from RDF Lift webapp configuration currently in liftConfig.ttl
+ *
+ * @author Ryan Biggs
+ */
+public class ChatConfig extends KnownComponentImpl {
+
+	public List<ChatConfigResource> myCCRs = new ArrayList<ChatConfigResource>();
+
+	public static class Builder extends DynamicCachingComponentAssembler<ChatConfig> {
+
+		public Builder(Resource builderConfRes) {
+			super(builderConfRes);
+		}
+
+		@Override
+		protected void initExtendedFieldsAndLinks(ChatConfig mcc, Item configItem, Assembler assmblr,
+				Mode mode) {
+			logInfo("ChatConfig.initExtendedFieldsAndLinks()-BEGIN");
+			Set<Item> resourceItems = ItemFuncs.getLinkedItemSet(configItem, ChatConfigNames.P_config);
+			for (Item ji : resourceItems) {
+				ChatConfigResource ccr = new ChatConfigResource(ji);
+				logInfo("Adding ChatConfigResource in ChatConfig: " + ccr);
+				mcc.myCCRs.add(ccr);
+			}
+		}
+
+		public static void clearCache() {
+			clearCacheFor(ChatConfig.Builder.class);
+		}
+	}
+	private static String UNIT_TEST_RDF_PATH = "metadata/chatbird/cogbotZenoAmazonEC.ttl";
+
+	public static void main(String args[]) {
+		BasicDebugger bd = new BasicDebugger();
+		bd.logInfo("starting ChatConfig test");
+		String triplesPath = UNIT_TEST_RDF_PATH;
+		// AssemblerUtils.ensureClassLoaderRegisteredWithJenaFM(AssemblyTest.class.getClassLoader());
+		bd.logInfo("Loading triples from path: " + triplesPath);
+		Set<Object> loadedStuff = AssemblerUtils.buildAllObjectsInRdfFile(triplesPath);
+		bd.logInfo("Loaded " + loadedStuff.size() + " objects");
+		for (Object o : loadedStuff) {
+			bd.logInfo("Loaded: " + o);
+		}
+		bd.logInfo("=====================================================================");
+
+	}
+}
---
+++ src_java_core/org/cogchar/bind/lift/ChatConfigNames.java
@@ -0,0 +1,32 @@
+/*
+ *  Copyright 2012 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+package org.cogchar.bind.lift;
+
+import org.appdapter.api.trigger.BoxAssemblyNames;
+
+/**
+ * @author Ryan Biggs
+ */
+public class ChatConfigNames extends BoxAssemblyNames {
+
+	public static final String NS_CgcChC = "http://www.cogchar.org/chat/config#";
+	public static final String partial_P_config = "config";
+	public static final String P_config = NS_CgcChC + partial_P_config;
+	public static final String P_entry = NS_CgcChC + "hasEntry";
+	public static final String P_name = NS_CgcChC + "hasName";
+	public static final String P_url = NS_CgcChC + "hasURL";
+	public static final String N_cogbotConvoUrl = "sendHeardAndGetSaid";
+}
