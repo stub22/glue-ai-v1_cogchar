86c2c5f8871751669f0cd19f4336fd0eeece3898
ryanbiggs <ryanbiggs@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-09-02 06:21:14 +0000

    Added new web app control, LOGINFORM, which provides basic ID and password input capability.

---
+++ trunk/maven/org.cogchar.bundle.bind.lift/src/main/scala/org/cogchar/lifter/snippet/LoginForm.scala
@@ -0,0 +1,90 @@
+/*
+ *  Copyright 2012 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cogchar.lifter {
+  package snippet {
+
+	import scala.xml._
+	import net.liftweb._
+	import http._
+	import common._
+	import js._
+	import JsCmds._
+	import JE._
+	import net.liftweb.http.js.JsCmd
+	import net.liftweb.util._
+	import Helpers._
+	import net.liftweb.http.SHtml._
+	import org.cogchar.lifter.model.PageCommander
+	import S._
+	
+	object LoginForm {
+	  val labelIdPrefix = "loginformlabel_"
+	  val textBoxIdPrefix = "login_in_"
+	  val defaultText1 = "" // Default text in the entry boxes can be set here if desired, but probably not
+	  val defaultText2 = ""
+	  val blankId: Int = -1
+	  val textMap = scala.collection.mutable.HashMap(blankId -> ("No label text found", "No label text found")) // Map to hold the labels for each form handled by this snippet, plus default
+	  
+	  def makeForm(label1: String, label2: String, submitLabel: String, idNum: Int): NodeSeq = {
+		textMap(idNum) = (label1, label2)
+		val formIdforHtml: String = idNum.toString
+		val labelId1: String = labelIdPrefix + formIdforHtml + "A"// We need unique IDs here, because JavaScript may be updating the label after post [future expansion]
+		val labelId2: String = labelIdPrefix + formIdforHtml + "B"
+		val inputId1: String = textBoxIdPrefix + formIdforHtml + "A"// JavaScript may want to do things to the input boxes too, like clear them
+		val inputId2: String = textBoxIdPrefix + formIdforHtml + "B"
+		<form class="lift:form.ajax"><lift:LoginForm formId={formIdforHtml}><div class="labels" id={labelId1}></div><input id={inputId1}/><div class="labels" id={labelId2}></div><input id={inputId2}/><br/><input type="submit" value={submitLabel}/></lift:LoginForm></form>
+	  }
+	}
+
+	class LoginForm extends StatefulSnippet with Logger {
+	  var text1: String = LoginForm.defaultText1
+	  var text2: String = LoginForm.defaultText2
+	  var formId: Int = LoginForm.blankId
+	  lazy val textFormInstanceLabel1 = LoginForm.labelIdPrefix + formId + "A"
+	  lazy val textFormInstanceLabel2 = LoginForm.labelIdPrefix + formId + "B"
+	  lazy val textBoxInstanceLabel1 = LoginForm.textBoxIdPrefix + formId + "A"
+	  lazy val textBoxInstanceLabel2 = LoginForm.textBoxIdPrefix + formId + "B"
+	 
+	  def dispatch = {case "render" => render}	  
+	  
+	  def render = {
+   
+		def process(): JsCmd = {
+		  info("Input text for form #" + formId + ": " + text1 + "; [password hidden]")
+		  val processThread = new Thread(new Runnable { // A new thread to call back into PageCommander to make sure we don't block Ajax handling
+			  def run() {
+				PageCommander.multiTextInputMapper(formId, Array(text1, text2)) // Let PageCommander know about the text so it can figure out what to do with it
+			  }
+			})
+		  processThread.start
+		  // Clear text in input boxes
+		  SetValById(textBoxInstanceLabel1, "") &  SetValById(textBoxInstanceLabel2, "")
+		}
+		
+		formId = (S.attr("formId") openOr "-1").toInt
+		val labelSelectorText1: String = "#"+textFormInstanceLabel1+" *"
+		val labelSelectorText2: String = "#"+textFormInstanceLabel2+" *"
+		val boxSelectorText1: String = "#"+textBoxInstanceLabel1
+		val boxSelectorText2: String = "#"+textBoxInstanceLabel2
+		labelSelectorText1 #> LoginForm.textMap(formId)._1 & labelSelectorText2 #> LoginForm.textMap(formId)._2 &
+		boxSelectorText1 #> (SHtml.text(text1, text1 = _, "id" -> textBoxInstanceLabel1)) &
+		boxSelectorText2 #> (SHtml.password(text2, text2 = _, "id" -> textBoxInstanceLabel2) ++ SHtml.hidden(process))
+	  }
+	}
+
+  }
+}
