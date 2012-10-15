786908f0cbd3ab9b1102ce12f0e0c5cf7a985ff8
Ryan Biggs <ryan@robokindrobots.com>
2012-10-15 05:37:56 +0000

    Lifter: Refactoring in ToggleButton to take advantage of refactored methods in ControlToggler. Minor additional refactoring in ControlToggler. Converted ControlToggler to a Scala object, since it consists solely of stateless helper methods.

--- maven/org.cogchar.bundle.bind.lift/src/main/scala/org/cogchar/lifter/model/ControlToggler.scala
+++ maven/org.cogchar.bundle.bind.lift/src/main/scala/org/cogchar/lifter/model/ControlToggler.scala
@@ -18,6 +18,7 @@ package org.cogchar.lifter.model
 
 import org.appdapter.core.name.FreeIdent
 import org.cogchar.bind.lift.ControlConfig
+import scala.xml.NodeSeq
 
 // We don't want control-specific logic in PageCommander, but the toggle behavior is handled in a fundamentally
 // special way. (Dual actions are stored in special map toggleButtonFullActionMap and controlDefMap is updated with
@@ -27,7 +28,7 @@ import org.cogchar.bind.lift.ControlConfig
 // toggle behavior if desired by simply registering in toggleButtonMap.
 // Really the way toggle is implemented is pretty ugly in general, maybe it will get better...
 
-class ControlToggler {
+object ControlToggler {
   
   def toggle(appState:LifterState, sessionId:String, slotNum: Int) {
 	if (appState.controlDefMap(sessionId) contains slotNum) {
@@ -35,7 +36,7 @@ class ControlToggler {
 		if (appState.toggleButtonMap(sessionId) contains slotNum) {
 		  val state = appState.toggleButtonMap(sessionId)(slotNum)
 		  setSingularAction(appState, sessionId, slotNum, state)
-		  setSingularControlState(sessionId, slotNum, appState.controlDefMap(sessionId)(slotNum), !state)
+		  getSingularControlXmlAndRender(sessionId, slotNum, appState.controlDefMap(sessionId)(slotNum), !state)
 		  appState.toggleButtonMap(sessionId)(slotNum) = !state
 		} else {
 		  ControlToggler.togglerMapError(sessionId, slotNum, "toggleButtonMap")
@@ -57,33 +58,33 @@ class ControlToggler {
 			val actionIdent = control.action
 			if (ActionStrings.p_liftvar.equals(PageCommander.getUriPrefix(actionIdent))
 				&& varName.equals(actionIdent.getLocalName)) {  
-			  setSingularControlState(sessionId, slotNum, control, state)
+			  getSingularControlXmlAndRender(sessionId, slotNum, control, state)
 			  appState.toggleButtonMap(sessionId)(slotNum) = state
 			}
 		  })
 	  })
   }
   
-  private def setSingularControlState(sessionId:String, slotNum:Int, control:ControlConfig, state:Boolean) {
+  def getSingularControlXml(sessionId:String, slotNum:Int, control:ControlConfig, state:Boolean): NodeSeq = {
 	val newControl = new ControlConfig();
 	newControl.controlType = control.controlType
 	newControl.text = ControlToggler.getSubstringForToggleState(control.text, state)
 	newControl.style = ControlToggler.getSubstringForToggleState(control.style, state)
 	newControl.resource = ControlToggler.getSubstringForToggleState(control.resource, state)
-	PageCommander.setControl(sessionId, slotNum, PageCommander.getXmlForControl(sessionId, slotNum, newControl))
+	PageCommander.getXmlForControl(sessionId, slotNum, newControl)
   }
   
-  private def setSingularAction(appState:LifterState, sessionId:String, slotNum:Int, state:Boolean) {
+  def getSingularControlXmlAndRender(sessionId:String, slotNum:Int, control:ControlConfig, state:Boolean) {
+	PageCommander.setControl(sessionId, slotNum, getSingularControlXml(sessionId, slotNum, control, state))
+  }
+  
+  def setSingularAction(appState:LifterState, sessionId:String, slotNum:Int, state:Boolean) {
 	val actionItem = 
 	  ControlToggler.getSubstringForToggleState(appState.toggleButtonFullActionMap(sessionId)(slotNum).getLocalName,
-		state, ActionStrings.multiCommandSeparator)
+												state, ActionStrings.multiCommandSeparator)
 	val actionUriPrefix = PageCommander.getUriPrefix(appState.controlDefMap(sessionId)(slotNum).action)
 	appState.controlDefMap(sessionId)(slotNum).action = new FreeIdent(actionUriPrefix + actionItem, actionItem)
   }
-  
-}
-
-object ControlToggler {
   
   def getStateIndex(state:Boolean) = if (state) 1 else 0
   
@@ -99,7 +100,7 @@ object ControlToggler {
   
   private def togglerMapError(sessionId:String, slotNum:Int, mapName:String) {
 	error("ControlToggler called for slotNum " + slotNum + " of session " + sessionId + 
-				", but no entry found in " + mapName)
+		  ", but no entry found in " + mapName)
   }
 
 }
