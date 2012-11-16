a96fbb21f7e42bdc83bb2c65376ecfe6ccbb03fa
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-11-16 03:15:36 +0000

    New lib project provides API code for accessing Cogchar features on a server, using SPARQL (+ Update) over HTTP.  Some of the guts will later be pushed into Appdapter,  but we can revise early prototypes faster at this layer, for now.
    First rev of TestOuterClientSOH prints SPARQL client results for "people born in 
    Germany before 1900", from DBPedia.org server.

---
+++ trunk/maven/org.cogchar.lib.outer.client/src/main/java/org/cogchar/outer/client/TestOuterClientSOH.java
@@ -0,0 +1,50 @@
+package org.cogchar.outer.client;
+import com.hp.hpl.jena.query.Query;
+import com.hp.hpl.jena.query.QueryFactory;
+import com.hp.hpl.jena.query.QueryExecutionFactory;
+import com.hp.hpl.jena.query.QueryExecution;
+import com.hp.hpl.jena.query.Dataset;
+import com.hp.hpl.jena.query.DatasetFactory;
+import com.hp.hpl.jena.query.Syntax;
+import com.hp.hpl.jena.query.ResultSet;
+import com.hp.hpl.jena.query.ResultSetFormatter;
+import com.hp.hpl.jena.query.ResultSetFactory;
+
+import com.hp.hpl.jena.query.ResultSetRewindable;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.sparql.util.graph.GraphFactory;
+import org.appdapter.bind.rdf.jena.model.JenaModelUtils;
+/**
+ * Hello world!
+ */
+
+public class TestOuterClientSOH {
+
+	public static String	repoServerRootURL = "http://localhost:8080/cgc_repo/";
+	public static String	sparqlServiceURL = "http://dbpedia.org/sparql";
+	public static void main(String[] args) {
+		System.out.println("QueryText=\n" + dbpQ);
+		// now creating query object
+		Query parsedQuery = QueryFactory.create(dbpQ);
+		String queryBaseURI = null;
+		Syntax queryFileSyntax = Syntax.syntaxSPARQL;
+		// Query parsedQuery = QueryFactory.read(queryFileURL, null, Syntax.syntaxSPARQL); // , queryBaseURI, queryFileSyntax);
+		// initializing queryExecution factory with remote service.
+		// **this actually was the main problem I couldn't figure out.**
+		QueryExecution qExc = QueryExecutionFactory.sparqlService(sparqlServiceURL, parsedQuery);
+		ResultSet resSet = qExc.execSelect();
+		ResultSetRewindable rewindableResSet = ResultSetFactory.makeRewindable(resSet);
+		String resultXML = ResultSetFormatter.asXMLString(rewindableResSet);		
+		System.out.println("ResultSet as XML: \n" + resultXML);
+	}
+static String dbpQ =
+"PREFIX dbo: <http://dbpedia.org/ontology/>\n" +
+"PREFIX foaf: <http://xmlns.com/foaf/0.1/>\n" +
+"PREFIX xsd: <http://www.w3.org/2001/XMLSchema#>\n" + 
+"PREFIX : <http://dbpedia.org/resource/>\n" +
+"SELECT ?name ?birth ?death ?person WHERE {\n" +
+		"?person dbo:birthPlace :Berlin .   ?person dbo:birthDate ?birth .   ?person foaf:name ?name .\n" +
+		"?person dbo:deathDate ?death .   FILTER (?birth < '1900-01-01'^^xsd:date) }";
+
+}
