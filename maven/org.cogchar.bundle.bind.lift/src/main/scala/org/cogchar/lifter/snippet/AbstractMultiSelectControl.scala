c12658c466a3c8e17f6f88c48ad7ce6214c10a4e
ryanbiggs <ryanbiggs@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-12-07 05:50:49 +0000

    Lifter: Refactored LinkList control as extension of new trait AbstractMultiSelectControl. Added map to LifterState to hold information needed for snippet rendering via Comet in order to enable its removal from control objects.

---
+++ maven/org.cogchar.bundle.bind.lift/src/main/scala/org/cogchar/lifter/snippet/AbstractMultiSelectControl.scala
@@ -0,0 +1,123 @@
+/*
+ *  Copyright 2012 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cogchar.lifter.snippet
+
+import org.cogchar.lifter.model.handler.AbstractControlInitializationHandler
+import org.cogchar.bind.lift.ControlConfig
+import org.cogchar.lifter.model.{ActionStrings,LifterState,PageCommander}
+import org.cogchar.lifter.view.TextBox
+import net.liftweb._
+import net.liftweb.util._
+import http._
+import common._
+import js._
+import JsCmds._
+import JE._
+import Helpers._
+import net.liftweb.http.SHtml._
+import S._
+import scala.xml._
+import scala.collection.mutable.HashMap
+
+trait AbstractMultiSelectControlObject extends AbstractControlInitializationHandler with Logger {
+  
+  protected def handleHere(state:LifterState, sessionId:String, slotNum:Int, control:ControlConfig): NodeSeq =  {
+	// From the RDF "text" value we assume a comma separated list with the first item the title and the rest option labels
+	val textItems = control.text.split(ActionStrings.stringAttributeSeparator)
+	val titleText = textItems(0)
+	val labelItems = textItems.tail
+	makeMultiControl(titleText, labelItems, sessionId, slotNum, state)
+  }
+  
+  val titlePrefix: String
+  
+  final def makeMultiControl(titleText: String, labelList:Array[String], sessionId:String, slotNum: Int, state:LifterState): NodeSeq = {
+	initializeMaps(state, titleText, labelList, sessionId, slotNum);
+	makeMultiControlImpl(titleText, labelList, sessionId, slotNum)
+  }
+  
+  def makeMultiControlImpl(titleText: String, labelList:Array[String], sessionId:String, slotNum: Int): NodeSeq
+  
+  def initializeMaps(state:LifterState, titleText: String, labelList:Array[String], sessionId:String, slotNum:Int) {
+	val controlData = new MultiSelectControlData
+	controlData title = titleText
+	controlData labels = labelList
+	state.getSnippetDataMapForSession(sessionId)(slotNum) = controlData
+  }
+}
+
+trait AbstractMultiSelectControl extends StatefulSnippet with Logger {
+  var formId: Int = blankId
+  val blankId = -1
+  var sessionId: String = ""
+  
+  final def snippetData(sessionId:String) = PageCommander.hackIntoSnippetDataMap(sessionId)
+  
+  def getName: String
+  
+  def dispatch = {case "render" => render}
+  
+  def render(xhtml:NodeSeq): NodeSeq = {
+	
+	S.session match {
+	  case Full(myLiftSession) => {
+		  sessionId = myLiftSession.uniqueId
+		  formId = (S.attr("formId") openOr "-1").toInt
+		  var valid = false
+		  var selectors:CssSel = "i_eat_yaks_for_breakfast" #> "" // This is just to produce a "Null" CssSel so we can initialize this here, but not add any meaningful info until we have checked for valid form data. (As recommended by the inventor of Lift)
+		  var errorSeq: NodeSeq = NodeSeq.Empty
+		  snippetData(sessionId)(formId) match {
+			case snippetResources: MultiSelectControlData => {
+				if (formId != blankId) {
+				  valid = true
+				  selectors = generateSelectors(sessionId, formId, snippetResources)
+				} else {
+				  errorSeq = produceErrorMessages(getName + ".render cannot find a valid formId!")
+				}
+			  }
+			case _ => {
+				errorSeq = produceErrorMessages(getName + " cannot find snippet data in LifterState!")
+			}
+		  }
+		  if (valid) selectors.apply(xhtml) else errorSeq
+		}
+	  case _ => {
+		  produceErrorMessages(getName + " cannot get sessionId, not rendering!")
+		}
+	}
+	
+  }
+  
+  def produceErrorMessages(errorText:String): NodeSeq = {
+	error(errorText)
+	TextBox.makeBox(errorText, "", true)
+  }
+  
+  def process(result: String): JsCmd = {
+	info(getName + " says link number " + result + " on formId " + formId + " is selected in session " + sessionId)
+	PageCommander ! PageCommander.ControlMultiAction(sessionId, formId, result.toInt)
+	JsCmds.Noop
+  }
+  
+  def generateSelectors(sessionId:String, formId:Int, snippetResources:MultiSelectControlData): CssSel
+}
+
+class MultiSelectControlData {
+  var title: String = null
+  var labels: Array[String] = null
+}
+
--- maven/org.cogchar.bundle.bind.lift/src/main/scala/org/cogchar/lifter/snippet/LinkList.scala
+++ maven/org.cogchar.bundle.bind.lift/src/main/scala/org/cogchar/lifter/snippet/LinkList.scala
@@ -30,50 +30,22 @@ package org.cogchar.lifter {
 	import net.liftweb.http.SHtml._
 	import org.cogchar.bind.lift.ControlConfig
 	import org.cogchar.lifter.model.{ActionStrings,LifterState,PageCommander}
-	import org.cogchar.lifter.model.handler.AbstractControlInitializationHandler
-	import org.cogchar.lifter.view.TextBox
 	import S._
 	import scala.collection.mutable.{Buffer,HashMap}
 
-	// Needs to be refactored as subclass of a common superclass to this and other "MultiSelect" controls
-	// Needs lots of refactoring in general -- ugly stuff in here at moment and lots of room for functionality improvements!
-	// But we'll get it going for the moment so we can use it for lists of Cogchar triggerables, and keep improving it later.
-	object LinkList extends AbstractControlInitializationHandler with Logger {
+	object LinkList extends AbstractMultiSelectControlObject {
 	  
 	  val matchingName = "LINKLIST"
 	  
 	  // The maximum number of items which will be displayed in a single column of "links"
 	  // Probably should come from RDF config in some way ultimately
 	  final val maxPerColumn = 20;
-  
-	  protected def handleHere(state:LifterState, sessionId:String, slotNum:Int, control:ControlConfig): NodeSeq =  {
-		// From the RDF "text" value we assume a comma separated list with the first item the title and the rest link labels
-		// In practice, as currently used, link labels are generated for these controls by a liftquery action and fed into this
-		// method. Likely will refine how this is handled to make the mechanism flexible and clean.
-		val textItems = control.text.split(ActionStrings.stringAttributeSeparator)
-		val titleText = textItems(0)
-		val labelItems = textItems.tail
-		makeLinkList(titleText, labelItems, sessionId, slotNum)
-	  }
 	  
 	  val titlePrefix = "linkListTitle"
 	  val listId = "list_item"
-	  
-	  val blankId = -1
-	  
-	  // This stuff needs to go -- evil singleton state. Similarly in other "MultiSelect" controls
-	  // Needed to pass info to snippet instances, but there are surely better ways to do this in Lift
-	  val titleMap = new HashMap[String,HashMap[Int,String]]
-	  val labelMap = new HashMap[String,HashMap[Int,Array[String]]]
 
-	  def makeLinkList(titleText: String, labelList:Array[String], sessionId:String, idNum: Int): NodeSeq = {
-		if (!(titleMap contains sessionId)) {
-		  titleMap(sessionId) = new HashMap[Int,String]
-		  labelMap(sessionId) = new HashMap[Int,Array[String]]
-		}
+	  def makeMultiControlImpl(titleText: String, labelList:Array[String], sessionId:String, idNum: Int): NodeSeq = {
 		val formIdForHtml: String = idNum.toString
-		titleMap(sessionId)(idNum) = titleText
-		labelMap(sessionId)(idNum) = labelList
 		val titleId: String = titlePrefix + formIdForHtml
 		val numberOfColumns = (labelList.length/maxPerColumn.doubleValue).ceil.intValue max 1
 		val columnWidth = 100/numberOfColumns //percent
@@ -93,57 +65,28 @@ package org.cogchar.lifter {
 	  }
 	}
 	
-	class LinkList extends StatefulSnippet with Logger {
+	class LinkList extends StatefulSnippet with AbstractMultiSelectControl {
 	  
-	  var formId: Int = LinkList.blankId
-	  var sessionId: String = ""
-	  lazy val linkListInstanceTitleId = LinkList.titlePrefix + formId
+	  def getName: String = LinkList.matchingName
 	  
-	  def dispatch = {case "render" => render}
-	  
-	  def render(xhtml:NodeSeq): NodeSeq = {
-		
-		def process(result: String): JsCmd = {
-		  info("LinkList says link number " + result + " on formId " + formId + " is selected in session " + sessionId)
-		  PageCommander ! PageCommander.ControlMultiAction(sessionId, formId, result.toInt)
-		  JsCmds.Noop
+	  def generateSelectors(sessionId: String, formId: Int, textData:MultiSelectControlData): CssSel = {
+		val title = textData title
+		val labels = textData labels
+		val listLength = labels.length
+		info("LinkList rendering: title: " + title + "; labels.length=" + listLength.toString) // TEST ONLY
+		val controlInstanceTitleId = LinkList.titlePrefix + formId
+		val titleSelectorText: String = "#"+controlInstanceTitleId+" *"
+		val buttonTags = (for (i <- 0 until listLength) yield i.toString)// There may be a simplier Scala way to do this
+		var selectors = titleSelectorText #> title
+		for (buttonIndex <- 0 until listLength) {
+		  val itemId = LinkList.listId + formId.toString + "_" + buttonIndex.toString // Needs refactoring to eliminate repeated code
+		  val buttonHtml = SHtml.ajaxButton(labels(buttonIndex), () => process(buttonIndex.toString))
+		  // Something like this might work and allow for less button-looking links, but I haven't gotten this idea to work yet:
+		  //val buttonHtml:NodeSeq = <div onclick={SHtml.ajaxInvoke(() => process(buttonIndex.toString))._2}>{LinkList.labelMap(formId)(buttonIndex)}</div>
+		  selectors = selectors & ("#"+itemId+" *") #> buttonHtml
 		}
-		
-		S.session match {
-		  case Full(myLiftSession) => {
-			sessionId = myLiftSession.uniqueId
-			formId = (S.attr("formId") openOr "-1").toInt 
-			var valid = false
-			var selectors:CssSel = "i_eat_yaks_for_breakfast" #> "" // This is just to produce a "Null" CssSel so we can initialize this here, but not add any meaningful info until we have checked for valid formId. (As recommended by the inventor of Lift)
-			var errorSeq: NodeSeq = NodeSeq.Empty
-			if (LinkList.titleMap(sessionId).contains(formId)) {
-			  valid = true
-			  val titleSelectorText: String = "#"+linkListInstanceTitleId+" *"
-			  val buttonTags = (for (i <- 0 until LinkList.labelMap(sessionId)(formId).length) yield i.toString)// There may be a simplier Scala way to do this
-			  selectors = titleSelectorText #> LinkList.titleMap(sessionId)(formId)
-			  for (buttonIndex <- 0 until LinkList.labelMap(sessionId)(formId).length) {
-				val itemId = LinkList.listId + formId.toString + "_" + buttonIndex.toString // Needs refactoring to eliminate repeated code
-				val buttonHtml = SHtml.ajaxButton(LinkList.labelMap(sessionId)(formId)(buttonIndex), () => process(buttonIndex.toString))
-				// Something like this might work and allow for less button-looking links, but I haven't gotten this idea to work yet:
-				//val buttonHtml:NodeSeq = <div onclick={SHtml.ajaxInvoke(() => process(buttonIndex.toString))._2}>{LinkList.labelMap(formId)(buttonIndex)}</div>
-				selectors = selectors & ("#"+itemId+" *") #> buttonHtml
-			  }
-			} else {
-			  val errorText = "LinkList.render cannot find a valid formId! Reported formId: " + formId
-			  error(errorText)
-			  errorSeq = TextBox.makeBox(errorText, "", true)
-			}
-			if (valid) selectors.apply(xhtml) else errorSeq
-		  }
-		  case _ => {
-			val errorText = "LinkList cannot get sessionId, not rendering!"
-			error(errorText)
-			TextBox.makeBox(errorText, "", true)
-		  }
-		}
-		
-	  }  
+		selectors
+	  }
 	}
-
   }
 }
