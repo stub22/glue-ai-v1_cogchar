f4d152c9368647156e1129e167d3a1dbb1534215
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2012-12-15 01:36:51 +0000

    ThingActions are now shipped mostly correctly over SPARQL-Update by TestRemoteBitBoxes. "Type" of the goody-thing is no longer treated as a Param.

--- trunk/maven/org.cogchar.lib.outer.client/src/main/java/org/cogchar/outer/client/TestRemoteBitBoxes.java
+++ trunk/maven/org.cogchar.lib.outer.client/src/main/java/org/cogchar/outer/client/TestRemoteBitBoxes.java
@@ -16,6 +16,8 @@ import org.cogchar.api.thing.ThingActionSpec;
 import org.cogchar.impl.thing.ConcreteTVM;
 import org.cogchar.impl.thing.FancyThingModelWriter;
 import org.slf4j.Logger;
+
+import java.util.Random;
 /**
  * @author Stu B. <www.texpedient.com>
  */
@@ -24,18 +26,19 @@ public class TestRemoteBitBoxes  extends BasicDebugger {
 	AgentRepoClient	myAgentRepoClient = new AgentRepoClient();
 
 	public static void main(String[] args) {
+		Random ran = new Random();
 		TestRemoteBitBoxes test = new TestRemoteBitBoxes();
-		Ident boxOneID = test.makeOneBitBox();
-		test.updateGoodyLocation(boxOneID, 700.0f, 800.0f, 900.0f);
+		Ident boxOneID = test.makeOneBitBox(ran);
+		test.updateGoodyLocation(boxOneID, 700.0f, 800.0f, 900.0f, ran);
 		
 	}
 	public TestRemoteBitBoxes() {
 		forceLog4jConfig();
 	}
-	private static String goodyGraphID = "GG";
+	private static String goodyGraphQN = "ccrt:thing_sheet_22";
 	private static String boxBaseURI = "http://dummy.org/bitbox#num_";
 	
-	public Ident makeOneBitBox() {
+	public Ident makeOneBitBox(Random ran) {
 		BasicTypedValueMap btvm = new ConcreteTVM();
 		GoodyActionParamWriter gapw = new GoodyActionParamWriter(btvm);
 		
@@ -47,37 +50,41 @@ public class TestRemoteBitBoxes  extends BasicDebugger {
 		String dummyBoxURI = boxBaseURI + tstamp;
 		Ident dummyBoxID = new FreeIdent(dummyBoxURI);
 		
-		Ident actRecID = null;
+		Ident actRecID = new FreeIdent("action_#" + ran.nextInt());
 		Ident tgtThingID = dummyBoxID;
 		Ident actVerbID = GoodyNames.CREATE_URI;
 		Ident srcAgentID = null;
 		
 		BasicThingActionSpec btas = new BasicThingActionSpec(actRecID, tgtThingID, actVerbID, srcAgentID, btvm);
-		sendThingActionSpec(btas);
+
+		sendThingActionSpec(btas, ran);
 		return dummyBoxID;
 	}
-	public void updateGoodyLocation(Ident goodyID, float locX, float locY, float locZ) {
+	public void updateGoodyLocation(Ident goodyID, float locX, float locY, float locZ, Random ran) {
 		BasicTypedValueMap btvm = new ConcreteTVM();
 		GoodyActionParamWriter gapw = new GoodyActionParamWriter(btvm);
 		
 		gapw.putLocation(locX, locY, locZ);
+
 		
-		Ident actRecID = null;
+		Ident actRecID = new FreeIdent("action_#" + ran.nextInt());
 		Ident tgtThingID = goodyID;
 		Ident actVerbID = GoodyNames.MOVE_URI;
 		Ident srcAgentID = null;
 		
 		BasicThingActionSpec btas = new BasicThingActionSpec(actRecID, tgtThingID, actVerbID, srcAgentID, btvm);
-		sendThingActionSpec(btas);
+
+		sendThingActionSpec(btas, ran);
 	}
 	
-	public void sendThingActionSpec(ThingActionSpec actionSpec) {
+	public void sendThingActionSpec(ThingActionSpec actionSpec, Random ran) {
 		Logger log = getLogger();
 		log.info("Sending action spec: " + actionSpec);
-		
+
 		FancyThingModelWriter ftmw = new FancyThingModelWriter();
-		String updateTxt = ftmw.writeTASpecToString(actionSpec);
+		String updateTxt = ftmw.writeTASpecToString(actionSpec, goodyGraphQN, ran);
 		
-		logInfo("UpdateTxt:\n" + updateTxt);
+		// logInfo("UpdateTxt:\n" + updateTxt);
+		myAgentRepoClient.execRemoteSparqlUpdate(TestOuterClientSOH.glueUpdURL, updateTxt);
 	}
 }
--- trunk/src_scala_core/org/cogchar/impl/thing/FancyThingSparqlGen.scala
+++ trunk/src_scala_core/org/cogchar/impl/thing/FancyThingSparqlGen.scala
@@ -16,9 +16,7 @@
 
 package org.cogchar.impl.thing
 
-class FancyThingSparqlGen {
-	
-}
+
 import java.io.Reader;
 import java.util.Iterator;
 import org.appdapter.bind.csv.datmat.TestSheetReadMain;
@@ -43,8 +41,9 @@ import org.cogchar.api.thing.{TypedValueMap, ThingActionSpec}
 import org.appdapter.impl.store.{ModelClientImpl, ResourceResolver};
 import org.cogchar.blob.emit.{SparqlTextGen}
 
-class FancyThingModelWriter {
-	def writeParamsUsingWeakConvention(m : Model,  parentRes : Resource, tvm : TypedValueMap) : Unit = {
+class FancyThingModelWriter extends BasicDebugger {
+	
+	def writeParamsUsingStrongConvention(m : Model,  parentRes : Resource, tvm : TypedValueMap) : Unit = {
 		val mci = new ModelClientImpl(m);
 		val rr = new ResourceResolver(m, None);
 		// val tm = com.hp.hpl.jena.datatypes.TypeMapper.getInstance()
@@ -63,27 +62,109 @@ class FancyThingModelWriter {
 			m.add(stmt)
 		}
 	}
-	def writeTASpecToNewModel(tas : ThingActionSpec) : Model  = {
-		val model : Model = ModelFactory.createDefaultModel();
-		val actionSpec : Ident = tas.getActionSpecID();
-		val verb : Ident = tas.getVerbID();
+	val RDF_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+	val XSD_NS = "http://www.w3.org/2001/XMLSchema#"
+
+	val CCRT_NS = "urn:ftd:cogchar.org:2012:runtime#"
+	
+	val TA_NS = "http://www.cogchar.org/thing/action#"
+	val GOODY_NS = "urn:ftd:cogchar.org:2012:goody#"
+	
+	
+	import java.util.Random;
+	
+	def writeParamsUsingWeakConvention(mci : ModelClientImpl,  actionRes : Resource, tvm : TypedValueMap, ran: Random) : Unit = {
+		val m : Model = mci.getModel;
+
+		val rr = new ResourceResolver(m, None);
+
+		// rdf:type	ta:targetAction	ta:verb	ta:targetThing	ta:paramIdent	ta:paramValue
+		
+		val paramLabelProp : Property = rr.findOrMakeProperty(m, TA_NS + "paramIdent")
+		val paramValueProp : Property = rr.findOrMakeProperty(m, TA_NS + "paramValue")
+		val rdfTypeProp : Property = rr.findOrMakeProperty(m, RDF_NS + "type")
+		val targetActionProp  : Property = rr.findOrMakeProperty(m, TA_NS + "targetAction")
+		
+		val ptRes : Resource = rr.findOrMakeResource(m, CCRT_NS + "ThingActionParam")
+		
+		// val tm = com.hp.hpl.jena.datatypes.TypeMapper.getInstance()
+		val nameIter : Iterator[Ident]  = tvm.iterateKeys();
+		
+		val random = new java.util.Random
+		val pBaseName = "tap_" + ran.nextInt + "_";
+		var paramNum = 1;
+		while (nameIter.hasNext()) {
+			val pName : String = pBaseName + paramNum;
+			val pRes : Resource = mci.makeResourceForURI(CCRT_NS + pName);
+			
+			val paramLabelID : Ident = nameIter.next();
+			// val paramProp : Property = rr.findOrMakeProperty(m, paramID.getAbsUriString)
+			val paramLabelRes : Resource = mci.makeResourceForIdent(paramLabelID)
+			val pvRaw = tvm.getRaw(paramLabelID)
+			val pvNode : RDFNode = pvRaw match  {
+				case idVal: Ident =>  mci.makeResourceForIdent(idVal)
+				case other =>  m.createTypedLiteral(other)
+			}
+			val ptStmt = m.createStatement(pRes, rdfTypeProp, ptRes) 
+			val paStmt = m.createStatement(pRes, targetActionProp, actionRes) 
+			val plStmt = m.createStatement(pRes, paramLabelProp, paramLabelRes) 
+			val pvStmt = m.createStatement(pRes, paramValueProp, pvNode)
+			
+			m.add(ptStmt);
+			m.add(paStmt);
+			m.add(plStmt);
+			m.add(pvStmt);
+			paramNum = paramNum + 1
+		}
+	}
+	def writeTASpecToNewModel(tas : ThingActionSpec, ran: Random) : Model  = {
+		val m : Model = ModelFactory.createDefaultModel();
+		val actionSpecID : Ident = tas.getActionSpecID();
+		val verbID : Ident = tas.getVerbID();
 		val targetThing : Ident = tas.getTargetThingID();
 		val srcAgentID : Ident	= tas.getSourceAgentID();
 		val tvm : TypedValueMap = tas.getParamTVM();
-		
-		val mci = new ModelClientImpl(model);
-		val thingRes : Resource = mci.makeResourceForIdent(targetThing)
-		
-		writeParamsUsingWeakConvention(model, thingRes, tvm)
-		println("thingRes=" + thingRes)
-		println("model after write=" + model)
-		model;
 
+		val mci = new ModelClientImpl(m);
+		val rr = new ResourceResolver(m, None);
+		
+		val rdfTypeProp : Property = rr.findOrMakeProperty(m, RDF_NS + "type")
+		val verbProp  : Property = rr.findOrMakeProperty(m, TA_NS + "verb")
+		val targetThingProp  : Property = rr.findOrMakeProperty(m, TA_NS + "targetThing")		
+
+		val taTypeRes : Resource =  rr.findOrMakeResource(m, CCRT_NS + "ThingAction")
+		
+		val actionSpecRes : Resource = mci.makeResourceForIdent(actionSpecID)
+		val thingRes : Resource = mci.makeResourceForIdent(targetThing)
+		val verbRes : Resource = mci.makeResourceForIdent(verbID)
+
+
+		// Write statements for TAS top facts
+		// rdf:type	ta:verb	ta:targetThing
+		val actTypeStmt = m.createStatement(actionSpecRes, rdfTypeProp, taTypeRes)
+		val actVerbStmt = m.createStatement(actionSpecRes, verbProp, verbRes)
+		val actThingStmt = m.createStatement(actionSpecRes, targetThingProp, thingRes)
+		
+		m.add(actTypeStmt)
+		m.add(actVerbStmt)
+		m.add(actThingStmt)
+		
+		writeParamsUsingWeakConvention(mci, actionSpecRes, tvm, ran)
+		println("thingRes=" + thingRes)
+		println("model after write=" + m)
+		m;
 	}
 	import java.io.ByteArrayOutputStream;	
 	
-	def writeTASpecToString (tas : ThingActionSpec) : String = {
-		val specModel  = writeTASpecToNewModel(tas)
+	def writeTASpecToString (tas : ThingActionSpec, tgtGraphQN : String, ran : Random) : String = {
+		val specModel : Model  = writeTASpecToNewModel(tas, ran)
+		
+		specModel.setNsPrefix("rdf", RDF_NS);
+		specModel.setNsPrefix("xsd" , XSD_NS)
+		
+		specModel.setNsPrefix("ccrt" , CCRT_NS)
+		specModel.setNsPrefix("ta" , TA_NS)
+		specModel.setNsPrefix("goody" , GOODY_NS)
 		
 		val baos : ByteArrayOutputStream = new ByteArrayOutputStream();
 		val outLang = "TURTLE";
@@ -91,11 +172,14 @@ class FancyThingModelWriter {
 		val encoding = "UTF8";
 		val turtleTriples = baos.toString(encoding)
 		
+		val lastPreStart = turtleTriples.lastIndexOf("@prefix");
+		val lastPreEnd = turtleTriples.indexOf("\n", lastPreStart)
+		
+		val turtleTriplesBare = turtleTriples.substring(lastPreEnd)
+		
 		val stg = new SparqlTextGen(specModel);
 		
-		val tgtGraphQN = "ccrt:user_access_sheet_22";
-
-		val upRqTxt = stg.emitSingleGraphInsert(tgtGraphQN, turtleTriples);
+		val upRqTxt = stg.emitSingleGraphInsert(tgtGraphQN, turtleTriplesBare);
 		
 		upRqTxt;
 	}
