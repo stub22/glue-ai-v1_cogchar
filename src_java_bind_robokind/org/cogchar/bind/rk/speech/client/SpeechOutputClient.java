ef53a6a6c27156017b5d7334b91c02e8d85233c7
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-04-23 00:43:05 +0000

    GuardedBehavior scenes are loading (from spreadsheet), but not playing correctly yet.

--- src_java_bind_robokind/org/cogchar/bind/rk/speech/client/SpeechOutputClient.java
+++ src_java_bind_robokind/org/cogchar/bind/rk/speech/client/SpeechOutputClient.java
@@ -34,13 +34,14 @@ import org.robokind.api.speech.utils.DefaultSpeechJob;
  */
 public class SpeechOutputClient extends FancyTextPerfChan<SpeechJob> {
 
-	private SpeechService			myCachedSpeechSvc;
-	private OldeSpeechOutCtxWrap	myOldCtxWrap;
+	private SpeechService myCachedSpeechSvc;
+	private OldeSpeechOutCtxWrap myOldCtxWrap;
 
 	public SpeechOutputClient(BundleContext bundleCtx, Ident chanIdent) {
 		super(chanIdent);
 		myOldCtxWrap = new OldeSpeechOutCtxWrap(bundleCtx);
 	}
+
 	public SpeechOutputClient(SpeechService speechSvc, Ident chanIdent) {
 		super(chanIdent);
 		myCachedSpeechSvc = speechSvc;
@@ -49,35 +50,41 @@ public class SpeechOutputClient extends FancyTextPerfChan<SpeechJob> {
 	// Java thinks this method is "public", even though it's marked "protected" in the Scala.
 	// https://issues.scala-lang.org/browse/SI-6097
 	// Also, Java does not recognize the Throwable annotation.
-	@Override public void fancyFastCueAndPlay (FancyTextMedia ftm, FancyTextCursor cuePos, FancyTextPerf perf)  {
+	@Override
+	public void fancyFastCueAndPlay(FancyTextMedia ftm, FancyTextCursor cuePos, FancyTextPerf perf) {
 		String textStr = ftm.getFullText();
 		// TODO:  "Cue" within the text, according to cursor
 		// TODO:  Deliver progress updates to the perf.
 		SpeechJob sjob = _directlyStartSpeakingText(textStr);
 		registerOutJobForPerf(perf, sjob);
 	}
-	@Override public void updatePerfStatusQuickly(FancyTextPerf perf) {
+
+	@Override
+	public void updatePerfStatusQuickly(FancyTextPerf perf) {
 		// TODO:  Keep track of job associated with the performance, and use perf.markState (and even perf.markCursor)
 		boolean finished = false;
 		SpeechJob jobToCheck = getOutJobOrNull(perf);
 		if (jobToCheck != null) {
-		int speechJobStatus = jobToCheck.getStatus();
-		switch (speechJobStatus) {
-			case DefaultSpeechJob.CANCELED:
-			case DefaultSpeechJob.COMPLETE:
-				finished = true;
-		}
+			int speechJobStatus = jobToCheck.getStatus();
+			switch (speechJobStatus) {
+				case DefaultSpeechJob.CANCELED:
+				case DefaultSpeechJob.COMPLETE:
+					finished = true;
+			}
 		} else {
+			finished = true;
 			getLogger().error("Found no job for performance, marking STOPPED: {}", perf);
 		}
 		if (finished) {
 			getLogger().info("Marking performance stopped");
 			markPerfStoppedAndForget(perf);
-		}		
-		
+		}
+
 	}
 	// We want this method to be private, but currently it cannot be because _____
-	@Deprecated	public SpeechJob _directlyStartSpeakingText(String textStr) {
+
+	@Deprecated
+	public SpeechJob _directlyStartSpeakingText(String textStr) {
 		if (myCachedSpeechSvc != null) {
 			SpeechJob speechJob = myCachedSpeechSvc.speak(textStr);
 			return speechJob;
@@ -87,71 +94,49 @@ public class SpeechOutputClient extends FancyTextPerfChan<SpeechJob> {
 			return null;
 		}
 	}
-	@Deprecated	public void _directlyCancelAllRunningSpeechTasks() {
+
+	@Deprecated
+	public void _directlyCancelAllRunningSpeechTasks() {
 		if (myCachedSpeechSvc != null) {
 			// TODO:  Plug in code to kill speech jobs.
 			// myCachedSpeechSvc.???????
 		} else if (myOldCtxWrap != null) {
 			myOldCtxWrap.oldCancelAllRunningSpeechTasks();
-		}		
+		}
 	}
-
 }
-	/*
->     On 4/8/2013 5:55 PM, Matt wrote:
->         I just changed the SpeechService so the speak(String) method now returns a SpeechJob.
->         The SpeechJob has addListener(Listener<org.__jflux.api.core.playable.__Playable.PlayState>), this is
->         also a PlayState in robokind which you do not want to use.
->         You will be notified when the speech begins with RUNNING.  There can be a delay between calling
->         speak(String)
->         and the speech job starting if other text is already in the speech queue.  You may miss the RUNNING
->         notification by the time you get a SpeechJob and are able to add the listener.
->         When the speech is complete you will be notified with COMPLETED, and if it is canceled you will be
->         notified with ABORTED.
-> On Mon, Apr 8, 2013 at 7:23 PM, StuB22
-* >  >     So, possibly on a very short or empty speech job (e.g. "") , we could also
->     miss the "COMPLETED" notice?> 
->     If so, can we additionally synchronously query the SpeechJob for its status?
-      
-	 * On 4/8/2013 6:31 PM, Matt Stevenson wrote:> Yes, there is a getStatus() method.  It returns an int for a 
-	 * status as defined in DefaultSpeechJob (this was copied over from Robosteps).  I will leave it this way 
-	 * for the show and clean it up a little more next week.
-> I could also modify the speech job so if it is completed or canceled and a listener is added, it will be 
-> notified immediately.
-> 
-> Another potential problem is sending a speech command at the same time from a different instance.  The speech 
-> jobs would get confused about the events from the other commands, but you can send two speech commands from a 
-> single instance without a problem.
-> 
-
-//	Here are the methods we can call on SpeechEvent
-//	public long getTimestampMillisecUTC()
-
-	 * Returns the name of the event of this event.
-	 * @return name of the event of this event
-    public String getSpeechEventType();
-	* Returns the stream number for tts output the event originates from.
-	//   public Long getStreamNumber();
-	 * Returns the position of the speech request the event begins at.
-	//   public Integer getTextPosition();
-	 * Returns the number of characters the event covers.
-	 * @return number of characters the event covers
-	//   public Integer getTextLength()
-	 * Returns event data (usually phone or viseme id) associated with the start of the event.
-	//   public Integer getCurrentData();
-//    public Integer getNextData();
-	 * Returns any String data associated with the event (used for SAPI bookmark events).
-//    public String getStringData();
-	 * Returns the duration of the event in milliseconds. For word boundaries, this duration for speaking the word in
-	 * milliseconds. For phonemes and visemes, this is the duration of event in milliseconds.
-	//   public Integer getDuration();
-*/	
-
 /*
- * /** Note This more flexible override is allowed but unnecessary if we are satisfied with the fancy-typed(nonparametric) approach above.
-	@Override protected <Cursor, M extends Media<Cursor>, Time> void  fastCueAndPlay(M m, Cursor startCursor, 
-				BasicPerformance<Cursor, M, Time> perf) throws Throwable 
-
+ * > On 4/8/2013 5:55 PM, Matt wrote: > I just changed the SpeechService so the speak(String) method now returns a
+ * SpeechJob. > The SpeechJob has addListener(Listener<org.__jflux.api.core.playable.__Playable.PlayState>), this is >
+ * also a PlayState in robokind which you do not want to use. > You will be notified when the speech begins with
+ * RUNNING. There can be a delay between calling > speak(String) > and the speech job starting if other text is already
+ * in the speech queue. You may miss the RUNNING > notification by the time you get a SpeechJob and are able to add the
+ * listener. > When the speech is complete you will be notified with COMPLETED, and if it is canceled you will be >
+ * notified with ABORTED. > On Mon, Apr 8, 2013 at 7:23 PM, StuB22 > > So, possibly on a very short or empty speech job
+ * (e.g. "") , we could also > miss the "COMPLETED" notice?> > If so, can we additionally synchronously query the
+ * SpeechJob for its status?
+ *
+ * On 4/8/2013 6:31 PM, Matt Stevenson wrote:> Yes, there is a getStatus() method. It returns an int for a status as
+ * defined in DefaultSpeechJob (this was copied over from Robosteps). I will leave it this way for the show and clean it
+ * up a little more next week. > I could also modify the speech job so if it is completed or canceled and a listener is
+ * added, it will be > notified immediately. > > Another potential problem is sending a speech command at the same time
+ * from a different instance. The speech > jobs would get confused about the events from the other commands, but you can
+ * send two speech commands from a > single instance without a problem. >  *
+ * //	Here are the methods we can call on SpeechEvent //	public long getTimestampMillisecUTC()
+ *
+ * Returns the name of the event of this event. @return name of the event of this event public String
+ * getSpeechEventType(); Returns the stream number for tts output the event originates from. // public Long
+ * getStreamNumber(); Returns the position of the speech request the event begins at. // public Integer
+ * getTextPosition(); Returns the number of characters the event covers. @return number of characters the event covers
+ * // public Integer getTextLength() Returns event data (usually phone or viseme id) associated with the start of the
+ * event. // public Integer getCurrentData(); // public Integer getNextData(); Returns any String data associated with
+ * the event (used for SAPI bookmark events). // public String getStringData(); Returns the duration of the event in
+ * milliseconds. For word boundaries, this duration for speaking the word in milliseconds. For phonemes and visemes,
+ * this is the duration of event in milliseconds. // public Integer getDuration();
  */
 
-
+/*
+ * /** Note This more flexible override is allowed but unnecessary if we are satisfied with the
+ * fancy-typed(nonparametric) approach above. @Override protected <Cursor, M extends Media<Cursor>, Time> void
+ * fastCueAndPlay(M m, Cursor startCursor, BasicPerformance<Cursor, M, Time> perf) throws Throwable  *
+ */
