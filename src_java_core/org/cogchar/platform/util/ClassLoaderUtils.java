fef14d3df20bc593500cd1c4eca7b5a7561d3c41
logicmoo <logicmoo@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-06-25 20:41:41 +0000

    Cogchar ClassLoaderUtils calls Appdapters now.. though i was wrong about the cache (maybe) I think what it is is bundles isooate their classloaders from each other

--- src_java_core/org/cogchar/platform/util/ClassLoaderUtils.java
+++ src_java_core/org/cogchar/platform/util/ClassLoaderUtils.java
@@ -17,100 +17,33 @@
 package org.cogchar.platform.util;
 
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Dictionary;
-import java.util.Hashtable;
 import java.util.List;
+
 import org.osgi.framework.BundleContext;
-import org.osgi.framework.FrameworkUtil;
-import org.osgi.framework.InvalidSyntaxException;
-import org.osgi.framework.ServiceReference;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  * @author Stu B. <www.texpedient.com>
  */
 
 public class ClassLoaderUtils {
-    private final static Logger theLogger = LoggerFactory.getLogger(ClassLoaderUtils.class.getName());
-    
-    public final static String RESOURCE_CLASSLOADER_TYPE = "ResourceClassLoaderType";
-    public final static String ALL_RESOURCE_CLASSLOADER_TYPES = "*";
-    
-	public static URL findResourceURL (String path, List<ClassLoader> cLoaders) {
-		for (ClassLoader cl : cLoaders) {
-			// This method will first search the parent class loader for the resource; if the parent is null the path of
-			// the class loader built-in to the virtual machine is searched. That failing, this method will invoke 
-			// findResource(String) to find the resource.
-			URL res = cl.getResource(path);
-			if (res != null) {
-				return res;
-			}
-		}
-		return null;
+
+	public final static String RESOURCE_CLASSLOADER_TYPE = org.appdapter.core.boot.ClassLoaderUtils.RESOURCE_CLASSLOADER_TYPE;
+	public final static String ALL_RESOURCE_CLASSLOADER_TYPES = org.appdapter.core.boot.ClassLoaderUtils.ALL_RESOURCE_CLASSLOADER_TYPES;
+
+	public static URL findResourceURL(String path, List<ClassLoader> cLoaders) {
+		return org.appdapter.core.boot.ClassLoaderUtils.findResourceURL(path, cLoaders);
 	}
-    
-	public static ClassLoader findResourceClassLoader (String path, List<ClassLoader> cLoaders) {
-		for (ClassLoader cl : cLoaders) {
-			// This method will first search the parent class loader for the resource; if the parent is null the path of
-			// the class loader built-in to the virtual machine is searched. That failing, this method will invoke 
-			// findResource(String) to find the resource.
-			URL res = cl.getResource(path);
-			if (res != null) {
-				return cl;
-			}
-		}
-		return null;
+
+	public static ClassLoader findResourceClassLoader(String path, List<ClassLoader> cLoaders) {
+		return org.appdapter.core.boot.ClassLoaderUtils.findResourceClassLoader(path, cLoaders);
 	}
-    
-    public static void registerClassLoader(BundleContext context, ClassLoader loader, String resourceClassLoaderType){
-        if(context == null || loader == null){
-            return;
-        }
-        if(resourceClassLoaderType == null){
-            resourceClassLoaderType = "UNKNOWN";
-        }
-        Dictionary<String,String> props = new Hashtable<String, String>();
-        props.put(RESOURCE_CLASSLOADER_TYPE, resourceClassLoaderType);
-        context.registerService(ClassLoader.class.getName(), loader, props);
-    }
-        
-    public static List<ClassLoader> getFileResourceClassLoaders(BundleContext context, String resourceClassLoaderType){
-        List<ClassLoader> resourceLoaders = new ArrayList<ClassLoader>();
-        if(context == null){
-            return resourceLoaders;
-        }
-        if(resourceClassLoaderType == null || resourceClassLoaderType.isEmpty()){
-            resourceClassLoaderType = ALL_RESOURCE_CLASSLOADER_TYPES;
-        }
-        ServiceReference[] loaders = null;
-        String filter = "(" + RESOURCE_CLASSLOADER_TYPE + "=" + resourceClassLoaderType + ")";
-        try{
-             loaders = context.getServiceReferences(ClassLoader.class.getName(), filter);
-        }catch(InvalidSyntaxException ex){
-            theLogger.warn("Syntax error with file resource ClassLoader filter string: " + filter + ".");
-        }
-        if(loaders == null || loaders.length == 0){
-            return resourceLoaders;
-        }
-        for(ServiceReference ref : loaders){
-            ClassLoader l = getLoader(context, ref);
-            if(l != null){
-                resourceLoaders.add(l);
-            }
-        }
-        return resourceLoaders;
-    }
-    
-    private static ClassLoader getLoader(BundleContext context, ServiceReference ref){
-        if(context == null || ref == null){
-            return null;
-        }
-        Object obj = context.getService(ref);
-        if(obj == null || !ClassLoader.class.isAssignableFrom(obj.getClass())){
-            return null;
-        }
-        return (ClassLoader)obj;
-    }
+
+	public static void registerClassLoader(BundleContext context, ClassLoader loader, String resourceClassLoaderType) {
+		org.appdapter.core.boot.ClassLoaderUtils.registerClassLoader(context, loader, resourceClassLoaderType);
+	}
+
+	public static List<ClassLoader> getFileResourceClassLoaders(BundleContext context, String resourceClassLoaderType) {
+		return org.appdapter.core.boot.ClassLoaderUtils.getFileResourceClassLoaders(context, resourceClassLoaderType);
+	}
+
 }
