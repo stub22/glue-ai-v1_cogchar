abcaea071b84e2c3310f532b5b81aa9b4b08128b
matt.r.stevenson <matt.r.stevenson@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-07-12 20:40:53 +0000

    TAGuard checks TAs from a TAGraphChan

--- trunk/src_scala_core/org/cogchar/impl/scene/Guard.scala
+++ trunk/src_scala_core/org/cogchar/impl/scene/Guard.scala
@@ -24,8 +24,10 @@ import org.appdapter.core.log.{ BasicDebugger, Loggable }
 import org.appdapter.core.name.{ Ident }
 import org.cogchar.api.perform.{ Media, PerfChannel, Performance }
 import org.cogchar.impl.perform.basic.{ BasicPerformance }
-import org.cogchar.impl.perform.{ FancyTime, FancyPerformance };
+import org.cogchar.impl.perform.{ FancyTime, FancyPerformance }
+import org.cogchar.impl.channel.{ ThingActionGraphChan }
 import org.cogchar.api.thing._
+import scala.collection.JavaConverters._
 /**
  * @author Stu B. <www.texpedient.com>
  */
@@ -73,6 +75,8 @@ class PerfMonGuardSpec(val myUpstreamStepID: Ident, val myStateToMatchOrExceed: 
   }
 }
 class ThingActionGuard(val mySpec: ThingActionGuardSpec) extends BasicDebugger with Guard {
+  private var mySatisfiedFlag : Boolean = false;
+  
   def makeTAFilter(myFilterID: Ident): ThingActionFilter = {
     //@todo use the assembler to make a TAF
     // ( ThingActionFilterBuilder )
@@ -85,15 +89,21 @@ class ThingActionGuard(val mySpec: ThingActionGuardSpec) extends BasicDebugger w
         val channelToCheck = fbs.getGraphChannel(mySpec.myListenChanID)
         val mustBeLaterThan = filterToCheck.getHasTStampGT();
         // FIXME:  We actually need to check for any state "equal or later" than the timestamp?
-        var aSpecIsGoodAsTheRealThingHere: ThingActionSpec = null
-
-        if (filterToCheck.test(aSpecIsGoodAsTheRealThingHere)) {
-          getLogger().debug("ThingAction {} is a MATCH for TA-Filter {}", aSpecIsGoodAsTheRealThingHere, filterToCheck)
-          true
-        } else {
-          getLogger().debug("ThingAction {} is NOT a match for TA-Filter {}", aSpecIsGoodAsTheRealThingHere, filterToCheck)
-          false
-        }
+        
+        channelToCheck match {                                 
+          case tagc: ThingActionGraphChan => {
+              val tasList : java.util.List[ThingActionSpec] = tagc.seeThingActions();
+              for (tas <- tasList.asScala) {
+                if (filterToCheck.test(tas)) {
+                  getLogger().debug("ThingAction {} is a MATCH for TA-Filter {}", tas, filterToCheck)
+                  return true
+                } else {
+                  getLogger().debug("ThingAction {} is NOT a match for TA-Filter {}", tas, filterToCheck)
+                }
+              }
+          }
+          case _ => getLogger().debug("GraphChannel {} is NOT a ThingActionGraphChannel, do not know how to get ThingAction", channelToCheck)             
+        }                                                   
       }
       case _ => {
         throw new RuntimeException("Coding error:  PerfMonitorGuard asked to check on a non-fancy scene")
