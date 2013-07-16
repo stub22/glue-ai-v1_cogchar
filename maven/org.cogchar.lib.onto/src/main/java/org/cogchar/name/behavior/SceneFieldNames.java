91cec90a665ec7c0c23113f0fe99ca5f1bd8d292
matt.r.stevenson <matt.r.stevenson@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-07-16 02:30:53 +0000

    ThingActionGuards are now built with steps.  However, there is a problem with the GraphChannels preventing them from working.

--- maven/org.cogchar.lib.onto/src/main/java/org/cogchar/name/behavior/SceneFieldNames.java
+++ maven/org.cogchar.lib.onto/src/main/java/org/cogchar/name/behavior/SceneFieldNames.java
@@ -49,6 +49,7 @@ public class SceneFieldNames {
 	public static String		P_waitForStart		= NS_ccScn + "waitForStart";
 	public static String		P_waitForEnd		= NS_ccScn + "waitForEnd";
 	public static String		P_waitForChan		= NS_ccScn + "waitForChan";
-	public static String		P_chanFilter		= NS_ccScn + "chanFilter";
+	public static String		P_chanFilter        = NS_ccScn + "chanFilter";
+	public static String		P_taGuard           = NS_ccScn + "taGuard";
 	
 }
