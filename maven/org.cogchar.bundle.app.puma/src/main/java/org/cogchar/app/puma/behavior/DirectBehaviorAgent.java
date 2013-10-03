8b3a9e21f905724b4b9c4defc207b4027df4787b
logicmoo <logicmoo@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-10-03 21:39:01 +0000

    Erase Map<Object,Object> from Dictionary

--- maven/org.cogchar.bundle.app.puma/src/main/java/org/cogchar/app/puma/behavior/DirectBehaviorAgent.java
+++ maven/org.cogchar.bundle.app.puma/src/main/java/org/cogchar/app/puma/behavior/DirectBehaviorAgent.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2013 by The Cogchar Project (www.cogchar.org).
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -38,7 +38,7 @@ import org.robokind.api.common.osgi.OSGiUtils;
 public class DirectBehaviorAgent extends PumaBehaviorAgent {
 	private PumaRobotMotionMapper myRobotMotionMapper;
 	private PumaSpeechOutputMapper mySpeechOutputMapper;
-	
+
 	public DirectBehaviorAgent(Ident agentID, BehaviorConfigEmitter bce)  {
 		super(agentID, bce);
 	}
@@ -60,12 +60,12 @@ public class DirectBehaviorAgent extends PumaBehaviorAgent {
 		FancyTextPerfChan bestAnimOutChan = myRobotMotionMapper.getBestAnimOutChan();
 		getLogger().warn("Found bestAnimOutChan {}", bestAnimOutChan);
 		thtr.registerPerfChannel(bestAnimOutChan);
-        
+
         BundleContext context = OSGiUtils.getBundleContext(WantsThingAction.class);
         if(context != null && WantsThingAction.class.isAssignableFrom(bestAnimOutChan.getClass())){
             Properties props = new Properties();
             props.put("thingActionChannelAgentId", myAgentID.getAbsUriString());
-            context.registerService(WantsThingAction.class.getName(), bestAnimOutChan, props);
+            context.registerService(WantsThingAction.class.getName(), bestAnimOutChan, (java.util.Dictionary)props);
         }
 	}
 	public void playBuiltinAnimNow(RobotAnimClient.BuiltinAnimKind baKind) {
@@ -75,7 +75,7 @@ public class DirectBehaviorAgent extends PumaBehaviorAgent {
 	public void sayTextNow(String txt) {
 		mySpeechOutputMapper._directlyStartSpeakingText(txt);
 	}
-	
+
 	@Override protected void doWiringPreStart(BundleContext optBunCtxForWiring, Theater thtr) {
 		super.doWiringPreStart(optBunCtxForWiring, thtr);
 	}
@@ -86,7 +86,7 @@ public class DirectBehaviorAgent extends PumaBehaviorAgent {
 			connectAnimOutChans(thtr);
 			connectSpeechOutputSvcs(optBunCtxForWiring, thtr);
 		}
-	}	
+	}
 	@Override protected void doStopOutputs () {
 		super.doStopOutputs();
 		getLogger().info("stopEverything for {} - Stopping Anim Jobs.", getAgentID());
@@ -96,6 +96,6 @@ public class DirectBehaviorAgent extends PumaBehaviorAgent {
 	}
 	@Override protected void doResetOutputs() {
 		getLogger().info("stopResetAndRecenter - Starting GOTO_DEFAULTS anim");
-		myRobotMotionMapper.playBuiltinAnimNow(RobotAnimClient.BuiltinAnimKind.BAK_GOTO_DEFAULTS);		
+		myRobotMotionMapper.playBuiltinAnimNow(RobotAnimClient.BuiltinAnimKind.BAK_GOTO_DEFAULTS);
 	}
 }
