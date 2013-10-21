3af6d18914b3ec7ed1267d041710bd29e6a0a18a
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-10-21 00:01:10 +0000

    New classes support simple computations over rectilinear "cell" space in 2D and 3D.

---
+++ test_java_core/org/cogchar/test/alpha/TestSpaceAPI.java
@@ -0,0 +1,29 @@
+/*
+ *  Copyright 2013 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cogchar.test.alpha;
+
+/**
+ * @author Stu B. <www.texpedient.com>
+ */
+
+public class TestSpaceAPI {
+	public static void main(String args[]) {
+		org.apache.log4j.BasicConfigurator.configure();
+		org.apache.log4j.Logger.getRootLogger().setLevel(org.apache.log4j.Level.ALL);
+		org.cogchar.api.space.CellSpaceTest.go();
+	}
+}	
