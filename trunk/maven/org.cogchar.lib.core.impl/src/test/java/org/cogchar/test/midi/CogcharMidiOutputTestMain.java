32757370b344f8e0099df523affbfa73a5a2c31f
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-11-01 17:12:19 +0000

    Renamed MIDI-test package.

--- trunk/maven/org.cogchar.lib.core.impl/src/test/java/org/cogchar/test/midi/CogcharMidiOutputTestMain.java
+++ trunk/maven/org.cogchar.lib.core.impl/src/test/java/org/cogchar/test/midi/CogcharMidiOutputTestMain.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.cogchar.test.mainers;
+package org.cogchar.test.midi;
 
 import java.io.File;
 import java.util.ArrayList;
