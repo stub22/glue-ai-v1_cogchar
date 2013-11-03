88ce157cfaee4e15f3ec4cc322f7daeda89bdcd7
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-11-03 02:16:10 +0000

    TextBox2D = Reusbable multiline-textbox with colored-transparent background, drawn on the 2D GUI surface.

---
+++ maven/org.cogchar.lib.render/src/test/java/org/cogchar/render/test/TextBox2D.java
@@ -0,0 +1,157 @@
+/*
+ *  Copyright 2013 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cogchar.render.test;
+
+import com.jme3.asset.AssetManager;
+import com.jme3.font.BitmapText;
+import com.jme3.material.Material;
+import com.jme3.material.RenderState;
+import com.jme3.math.ColorRGBA;
+import com.jme3.renderer.queue.RenderQueue;
+import com.jme3.scene.Geometry;
+import com.jme3.scene.Node;
+import com.jme3.scene.shape.Quad;
+import org.appdapter.core.log.BasicDebugger;
+import org.appdapter.core.name.Ident;
+import org.cogchar.render.sys.registry.RenderRegistryClient;
+
+/**
+ * @author Stu B. <www.texpedient.com>
+ */
+
+public class TextBox2D extends BasicDebugger {
+	private	static Material theBaseUnshadedMat;
+	// Remember:  Both nodes and geometries are spatials.  Spatial is an abstract class, not an interface.
+	// A BitmapText is a node, and thus also a spatial.
+	// The setMaterial method, and the setLocalTrans[++] are defined on *spatial* and all of its subclasses.
+	// A mesh is not a spatial or any of the other things mentioned above.
+	// Mesh has these direct subclasses for simpler shapes:  
+	// AbstractBox, Arrow, Curve, Cylinder, Dome, Grid, Line, ParticleMesh, PQTorus, Quad, SkeletonPoints, 
+	// SkeletonWire, Sphere, Surface, Torus, WireBox, WireFrustum, WireSphere
+	// When loading meshes from Ogre, we get some other private Mesh sub-class from that plugin MeshLoader.
+	
+	private	ColorRGBA	myQuadColor, myTextColor;
+	private	Node		myNode, myParentNode;
+	private BitmapText	myBmapTextSpat;
+	private Geometry	myQuadGeo;
+	private	Ident		myIdent;
+	private String		myTextString;
+	private	Integer		myX = 0, myY = 0, myWidth = 100, myHeight = 100;
+	private Float		myZOrder = -0.5f;
+	// private TextSpatialFactory	myTextSpatialFactory;
+	
+	public TextBox2D(Ident requiredID, String optInitText, ColorRGBA optInitTextColor, ColorRGBA optInitQuadColor) {
+		myIdent = requiredID;
+		if (optInitText != null) {
+			myTextString = optInitText;
+		} else {
+			myTextString = myIdent.getAbsUriString();
+		}
+		myQuadColor = optInitQuadColor;
+		myTextColor = optInitTextColor;
+	}
+	public void setCoordinates(Integer x, Integer y, Float zOrder, Integer width, Integer height) { 
+		myX = x;   myY = y;  myZOrder = zOrder;  myWidth = width;   myHeight = height;
+		applyTranslation();
+	}
+	private void applyTranslation() { 
+		if ((myX != null) && (myY != null)) { 
+			Node n = getMyNode();
+			n.setLocalTranslation(myX, myY, myZOrder);
+		}
+	}
+	private Node getMyNode() { 
+		if (myNode == null) {
+			myNode = new Node("TextBox2D-mainNode:" + myIdent.getLocalName());
+		}
+		return myNode;
+	}
+	public void setTextVal(String val) { 
+		myTextString = val;
+		myBmapTextSpat.setText(myTextString);
+		
+	}
+
+	public void setupContentsAndAttachToParent(Node parentNode, RenderRegistryClient rrc, AssetManager assetMgr) { 
+		Node mn = getMyNode();
+
+		String quadGeoName = "TextBox2D-mainNode:" + myIdent.getLocalName();
+		
+		// BEGIN - general form, unused
+		Material anyMat = getBaseMat(assetMgr);
+		// Here insert the mat.clone and modify operation, then pass that mat on the next line
+		Geometry junkGeoWithSomeMat = makeQuadGeoWithOptMat(quadGeoName, myWidth, myHeight, anyMat);
+		// END - general form
+		
+		// Begin - more specific form - quad has a unique material with an alpha-blended color.
+		ColorRGBA purplyTransparentColor = new ColorRGBA(0.5f, 0.0f, 0.7f, 0.5f);
+		myQuadGeo = makeQuadGeoWithBlendedMat(assetMgr, quadGeoName, myWidth, myHeight, purplyTransparentColor, RenderState.BlendMode.Alpha);
+		TextSpatialFactory tsf = new TextSpatialFactory(rrc);
+		float renderScale = 1.0f;
+		int textLineWrapWidthPixels = 90;
+		myBmapTextSpat = tsf.makeTextSpatial(myTextString, renderScale, RenderQueue.Bucket.Inherit, textLineWrapWidthPixels);
+		// We note that text runs downward, so we need to move the text up by about the height of the rect.
+		int leftMarginWidth = 5;
+		int topMarginHeight = 5;
+		myBmapTextSpat.setLocalTranslation(leftMarginWidth, myHeight - topMarginHeight, 0.5f);
+		mn.attachChild(myBmapTextSpat);
+		mn.attachChild(myQuadGeo);
+		attachToParentGuiNode(parentNode);	
+	}
+	private void attachToParentGuiNode(Node parentNode) {
+		myParentNode = parentNode;
+		Node mn = getMyNode();
+		myParentNode.attachChild(mn);
+		applyTranslation();
+	}
+		/*
+		 // http://hub.jmonkeyengine.org/javadoc/com/jme3/material/RenderState.BlendMode.html
+	  
+		 Additive - Additive blending.
+		 Alpha - Alpha blending, interpolates to source color from dest color using source alpha.
+		 AlphaAdditive -      Additive blending that is multiplied with source alpha.
+		 Color -     Color blending, blends in color from dest color using source color.
+		 Modulate -   Multiplies the source and dest colors.
+		 ModulateX2 -  Multiplies the source and dest colors then doubles the result.
+		 Off - No blending mode is used.
+		 PremultAlpha -  Premultiplied alpha blending, for use with premult alpha textures.
+		 */
+		
+	public static Geometry makeQuadGeoWithBlendedMat(AssetManager assetMgr, String name, int width, int height, ColorRGBA col, RenderState.BlendMode blendMode) {
+		Material baseMat = getBaseMat(assetMgr);
+		Material ourMat = baseMat.clone();
+		ourMat.getAdditionalRenderState().setBlendMode(blendMode);		
+		ourMat.setColor("Color", col);
+
+		return makeQuadGeoWithOptMat(name, width, height, ourMat);
+	} 
+	public static Geometry makeQuadGeoWithOptMat(String name, int width, int height, Material optMatToUseDirectly) { 
+		Geometry quadGeo = null;
+		quadGeo = new Geometry("name", new Quad(width, height));
+		if (optMatToUseDirectly != null) {
+			quadGeo.setMaterial(optMatToUseDirectly);
+		}
+		return quadGeo;
+	}	
+	private static Material getBaseMat(AssetManager assetMgr) { 
+		if (theBaseUnshadedMat == null) {
+			theBaseUnshadedMat = new Material(assetMgr, "Common/MatDefs/Misc/Unshaded.j3md");
+			theBaseUnshadedMat.setColor("Color", new ColorRGBA(0, 1.0f, 0, 0.5f));
+		}
+		return theBaseUnshadedMat;
+	}	
+}
