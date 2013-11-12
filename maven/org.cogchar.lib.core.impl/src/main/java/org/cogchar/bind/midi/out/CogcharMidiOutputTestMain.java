88cfa5f9a58471a891500259d7ce739b9a84cc9d
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2013-11-12 15:51:31 +0000

    Added Midi-output to the TrialBalloon.

--- maven/org.cogchar.lib.core.impl/src/main/java/org/cogchar/bind/midi/out/CogcharMidiOutputTestMain.java
+++ maven/org.cogchar.lib.core.impl/src/main/java/org/cogchar/bind/midi/out/CogcharMidiOutputTestMain.java
@@ -13,7 +13,7 @@
  *  See the License for the specific language governing permissions and
  *  limitations under the License.
  */
-package org.cogchar.test.midi;
+package org.cogchar.bind.midi.out;
 
 import java.io.File;
 import java.util.ArrayList;
