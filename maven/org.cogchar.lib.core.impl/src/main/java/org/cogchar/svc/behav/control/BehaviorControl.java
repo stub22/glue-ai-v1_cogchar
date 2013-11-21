36430d3a329de5fe2ad9b8531fde97342f5fc660
Jason Randolph Eads <jeads442@gmail.com>
2013-11-21 19:42:59 +0000

    Updating behavior control to smooth logic, add defensive programming. Added local references in LifterClientRegistration.

--- maven/org.cogchar.lib.core.impl/src/main/java/org/cogchar/svc/behav/control/BehaviorControl.java
+++ maven/org.cogchar.lib.core.impl/src/main/java/org/cogchar/svc/behav/control/BehaviorControl.java
@@ -30,36 +30,34 @@ import org.cogchar.api.thing.TypedValueMap;
  * Facilitates the control of scenes via lifter. Uses a ThingAction-provided key
  * to perform a lookup on a pair of ActionCallbackMaps, providing a convenient
  * trigger mechanism for events.
- * 
+ *
  * @author Jason Randolph Eads <jeads362@gmail.com>
  */
 public class BehaviorControl implements WantsThingAction {
-    
+
     static int mySceneEventIdCounter = 0;
-
     Logger theLogger = LoggerFactory.getLogger(BehaviorControl.class);
-
     // Maps containing references to the code to be triggered.
     private ActionCallbackMap mySceneActionCallbackMap;
     private ActionCallbackMap myAdminActionCallbackMap;
-    
+
     /**
      * Instance a BehaviorControl.
-     * 
+     *
      * @param sceneActionCallbackMap URI's mapped to playable scenes
      * @param adminActionCallbackMap URI's mapped to administrator actions
      */
     public BehaviorControl(
             ActionCallbackMap sceneActionCallbackMap,
-            ActionCallbackMap adminActionCallbackMap ) {
-        
+            ActionCallbackMap adminActionCallbackMap) {
+
         mySceneActionCallbackMap = sceneActionCallbackMap;
         myAdminActionCallbackMap = adminActionCallbackMap;
     }
-    
+
     /**
      * Perform the action referenced by the ThingAction, if any.
-     * 
+     *
      * @param actionSpec ThingAction that carries target data
      * @param srcGraphID Source that provided the ThingAction (extraneous)
      * @return Status of the TA after processing
@@ -68,47 +66,65 @@ public class BehaviorControl implements WantsThingAction {
     public ConsumpStatus consumeAction(
             ThingActionSpec actionSpec,
             Ident srcGraphID) {
-        
+
+        if (actionSpec == null) {
+            theLogger.warn("Null actionSpec passed to BehaviorControl for consumption.");
+            return ConsumpStatus.IGNORED;
+        }
+        if (srcGraphID == null) {
+            theLogger.warn("Null srcGraphID passed to BehaviorControl for consumption.");
+            return ConsumpStatus.IGNORED;
+        }
         theLogger.trace("BehaviorControl is considering action {}", actionSpec);
-        
+
         // Fire an event specified by the URI of the ThingAction.
         TypedValueMap paramTVM = actionSpec.getParamTVM();
-        if( paramTVM != null ) {
-            String sceneName = paramTVM
-                    .getAsIdent(WebUserActionNames.ACTION)
-                    .getLocalName();
+        if (paramTVM == null) {
+            theLogger.warn("Null param TVM found in actionSpec passed to BehaviorControl for consumption.");
+            return ConsumpStatus.IGNORED;
+        }
 
+        // Ensures TA is relevant.
+        Ident sceneNameID = paramTVM
+                .getAsIdent(WebUserActionNames.ACTION);
+        if( sceneNameID == null ) {
+            theLogger.trace("Did not find Ident on actionSpec for key: " + WebUserActionNames.ACTION.getAbsUriString() );
+            return ConsumpStatus.IGNORED;
+        }
+        String sceneName = sceneNameID.getLocalName();
+
+        if (myAdminActionCallbackMap != null) {
+            // Pull a admin action from the ACBM, if one with given name exists
             ActionListener adminActionListener = 
-                    myAdminActionCallbackMap == null ? null :
                     myAdminActionCallbackMap.getActionCallback(sceneName);
-            
-            if( adminActionListener != null ) {
+            if (adminActionListener != null) {
                 // Handle admin action.
-                ActionEvent event = 
+                ActionEvent event =
                         new ActionEvent(this, mySceneEventIdCounter++, "");
                 adminActionListener.actionPerformed(event);
                 theLogger.info("BehaviorControl is consuming admin action {}", actionSpec);
                 return ConsumpStatus.CONSUMED;
             }
-            else {
-                ActionListener sceneActionListener = 
-                        mySceneActionCallbackMap == null ? null :
-                        mySceneActionCallbackMap.getActionCallback(sceneName);
-                if(sceneActionListener != null ) {
-                    // Handle Scene Trigger action.
-                    ActionEvent event = new ActionEvent(
-                            this, 
-                            mySceneEventIdCounter++, 
-                            "");
-                    sceneActionListener.actionPerformed(event);
-                    theLogger.info("BehaviorControl is consuming scene action {}", actionSpec);
-                    return ConsumpStatus.CONSUMED;
-                }
+        }
+        else if ( mySceneActionCallbackMap != null ) {
+            // Pull a scene from the ACBM, if one with given name exists
+            ActionListener sceneActionListener = 
+                    mySceneActionCallbackMap.getActionCallback(sceneName);
+            if (sceneActionListener != null) {
+                // Handle Scene Trigger action.
+                ActionEvent event = new ActionEvent(
+                        this,
+                        mySceneEventIdCounter++,
+                        "");
+                sceneActionListener.actionPerformed(event);
+                theLogger.info("BehaviorControl is consuming scene action {}", actionSpec);
+                return ConsumpStatus.CONSUMED;
             }
         }
-        
+
         // Ignore any ThingAction not explicitly used
-        theLogger.trace("BehaviorControl ignored action {}", actionSpec);
+        theLogger.trace(
+                "BehaviorControl ignored action {}", actionSpec);
         return ConsumpStatus.IGNORED;
     }
 
