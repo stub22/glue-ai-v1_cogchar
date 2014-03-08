75124c1fb3c65f1285fa7771240f8bd2df10af91
mjacquote <mjacquote@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2014-03-08 19:55:31 +0000

    New PUMA code move to a new folder for now.

---
+++ maven/org.cogchar.bundle.app.new.puma/src/main/java/org/cogchar/app/puma/config/TriggerConfig.java
@@ -0,0 +1,46 @@
+package org.cogchar.app.puma.config;
+
+import org.cogchar.platform.trigger.CommandSpace;
+import org.cogchar.platform.trigger.BoxSpace;
+import org.appdapter.help.repo.RepoClient;
+/**
+ *
+ * @author robokind
+ */
+public class TriggerConfig {
+    
+    private CommandSpace commandSpace;
+    private BoxSpace boxSpace;
+    private RepoClient repoClient;
+    
+    
+    public CommandSpace getCommandSpace()
+    {
+        return commandSpace;
+    }
+    
+    public BoxSpace getBoxSpace()
+    {
+        return boxSpace;
+    }
+    
+    public RepoClient getRepoClient()
+    {
+        return repoClient;
+    }
+    
+    public void setRepoClient(RepoClient repo)
+    {
+        repoClient=repo;
+    }
+    
+    public void setBoxSpace(BoxSpace boxSpace)
+    {
+        this.boxSpace=boxSpace;
+    }
+    
+    public void setCommandSpace(CommandSpace commandSpace)
+    {
+        this.commandSpace=commandSpace;
+    }
+}
