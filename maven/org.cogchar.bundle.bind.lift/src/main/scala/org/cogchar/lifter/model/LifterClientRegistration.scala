aecd9d3ebe7190b89320c9fea013832e66793a3d
Jason Randolph Eads <jeads442@gmail.com>
2014-03-24 17:49:04 +0000

    This is an update to lifter. Added multi-user support, but it is not fully dynamic yet. Added automatic push to a start page after user registration. Retains backwards compatibility with old data.

--- maven/org.cogchar.bundle.bind.lift/src/main/scala/org/cogchar/lifter/model/LifterClientRegistration.scala
+++ maven/org.cogchar.bundle.bind.lift/src/main/scala/org/cogchar/lifter/model/LifterClientRegistration.scala
@@ -21,57 +21,115 @@ import org.appdapter.core.name.{FreeIdent,Ident}
 //import scala.collection.mutable.ArrayBuffer
 import org.cogchar.api.thing.{ThingActionSpec, WantsThingAction, TypedValueMap}
 import org.cogchar.api.thing.WantsThingAction.ConsumpStatus
+import org.cogchar.name.lifter.ActionStrings
+import org.cogchar.name.thing.ThingCN
+import org.slf4j.{Logger,LoggerFactory}
+
+import org.cogchar.api.fancy.{FancyThingModelWriter}
+import org.cogchar.impl.thing.basic.BasicThingActionSpecBuilderTempFunctions
+import org.cogchar.impl.thing.basic.{BasicThingActionSpec, BasicTypedValueMapWithConversion, BasicTypedValueMap};
+import org.cogchar.impl.thing.fancy.ConcreteTVM;
+import java.util.Random;
+import org.cogchar.outer.client.AgentRepoClient;
+import org.cogchar.outer.client.TestOuterClientSOH;
+
 
 class LifterClientRegistration extends WantsThingAction {
-    
-  //http://www.cogchar.org/lift/action/registration#
-    
-  private val lifterStudentRegistrationActionID : Ident =
-    new FreeIdent(
-      "http://www.cogchar.org/lift/action/registration#student");
+  val theLogger: Logger = LifterClientRegistration.theLogger
   
-//  private val lifterFacilitatorRegistrationActionID : Ident =
-//    new FreeIdent(
-//      "http://www.cogchar.org/lift/action/registration#facilitator");
-    
-    private val lifterActionID : Ident = 
-            new FreeIdent("http://www.cogchar.org/lift/user/action#action");
-
-  private val theStudentRegistrationLocalName : String = lifterStudentRegistrationActionID.getLocalName;
-//  private val theFacilitatorRegistrationLocalName : String = lifterFacilitatorRegistrationActionID.getLocalName;
-  
-  private val lifterSessionID :Ident = 
-    new FreeIdent("http://www.cogchar.org/lift/user/action#session");
-
   @Override
   def consumeAction(
     actionSpec:ThingActionSpec, srcGraphID:Ident ): ConsumpStatus = {
 
     val t:TypedValueMap = actionSpec.getParamTVM();
-    if(t.getAsIdent(lifterActionID) == null) {
-      return ConsumpStatus.IGNORED;
-    }
-    else{
-      // This ensures the TA is a student registration
-      val action :Ident = t.getAsIdent(lifterActionID);
-      if( action.getLocalName().equals(
-          theStudentRegistrationLocalName)) {
-                
-        // register the student's tablet's session
-        LifterClientRegistration.studentSessionID = 
-          t.getAsString(lifterSessionID);
+    
+    // Check if the TA matches registration for any of the users
+    val registrationAction:Ident = t.getAsIdent(
+      ActionStrings.LIFTER_ACTION);
+    
+    val registrationSession:String = t.getAsString(
+      ActionStrings.LIFTER_SESSION)
+    
+    theLogger.debug("checking possible registration action: {}", registrationAction)
+      
+    for( ID <- LifterClientRegistration.listOfRegistrationIDs ) {
+      
+      if( registrationAction.getLocalName().equals(ID)) {
+        
+        theLogger.debug("registration action detected: {}", registrationAction)
+    
+        // register the user's session
+        LifterClientRegistration.registerSession(
+          ID,
+          registrationSession)
+        
+        theLogger.info(
+          "User \""
+          + ID
+          + "\" registered with session # "
+          + registrationSession)
+        
         return ConsumpStatus.CONSUMED;
       }
-      return ConsumpStatus.IGNORED;
     }
+    return ConsumpStatus.IGNORED;
   }
 }
 
+
+
+/**
+ * Retains user session registrations for local code to reference lifter 
+ * browsers with.
+ */
 object LifterClientRegistration {
+  val theLogger: Logger = LoggerFactory.getLogger(classOf[LifterClientRegistration])
+  val ID: Ident = new FreeIdent(
+    "http://www.glue.ai/system/class/reference#"
+    + "org.cogchar.lifter.model.LifterClientRegistration");
   
-  def getCurrentStudentLifterSession(): String = {
-    return LifterClientRegistration.studentSessionID;
+  // The list from which registered users are indentified
+  val listOfRegistrationIDs = List(
+    ActionStrings.DEFAULT_REGISTRATION,
+    ActionStrings.STUDENT_REGISTRATION,
+    ActionStrings.FACILITATOR_REGISTRATION
+  )
+  
+  // Stores the lifter session for user, allowing communication with browser
+  private var mapUserToSession: collection.immutable.Map[String,String] =
+    collection.Map.empty;
+  
+  // The start page for a given user role
+  val mapRegistrationIDsToStartPageURIs = Map[String,Ident](
+    
+
+    ActionStrings.DEFAULT_REGISTRATION ->
+    ActionStrings.STUDENT_START_PAGE,
+    
+    ActionStrings.STUDENT_REGISTRATION ->
+    ActionStrings.STUDENT_START_PAGE,
+    
+    ActionStrings.FACILITATOR_REGISTRATION -> 
+    ActionStrings.FACILITATOR_START_PAGE
+  )
+  
+  private val LifterClientRegistrationID: Ident = new FreeIdent(
+    "http://www.cogchar.org/schema/crazyRandom#org.cogchar.lifter.model.LifterClientRegistration");
+  
+  def registerSession( userRegistationID:String, sessionID:String ) {
+    
+    // This is for backwards compatibility 
+    // may be removed once RDF data is updated
+    if( userRegistationID == ActionStrings.STUDENT_REGISTRATION ) {
+      LifterClientRegistration.mapUserToSession = 
+        mapUserToSession + ((ActionStrings.DEFAULT_REGISTRATION, sessionID))
+    }
+    
+    LifterClientRegistration.mapUserToSession = 
+      mapUserToSession + ((userRegistationID, sessionID))
   }
   
-  var studentSessionID : String = null;
+  def getLifterSession( userRegistationID :String ): String = {
+    return LifterClientRegistration.mapUserToSession(userRegistationID);
+  }
 }
