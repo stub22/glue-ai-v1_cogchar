2f41bfff7588a40dfd0b186dfe3b0fcb8d55b5f3
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2014-03-25 02:26:38 +0000

    Code cleanup, no  (intended) functional changes.

--- trunk/maven/org.cogchar.bundle.app.vworld.startup/src/main/java/org/cogchar/bundle/app/vworld/central/VWorldMapperLifecycle.java
+++ trunk/maven/org.cogchar.bundle.app.vworld.startup/src/main/java/org/cogchar/bundle/app/vworld/central/VWorldMapperLifecycle.java
@@ -16,6 +16,7 @@ import org.jflux.api.service.ServiceLifecycle;
 import org.cogchar.app.puma.config.PumaContextMediator;
 import org.appdapter.core.log.BasicDebugger;
 import org.appdapter.core.name.Ident;
+import org.appdapter.help.repo.RepoClient;
 import org.cogchar.app.puma.registry.PumaRegistryClient;
 import org.cogchar.app.puma.event.CommandEvent;
 import org.cogchar.app.puma.event.Updater;
@@ -23,12 +24,12 @@ import org.cogchar.app.puma.boot.PumaAppContext;
 
 public class VWorldMapperLifecycle extends BasicDebugger implements ServiceLifecycle<VWorldRegistry> {
 
-    private final static String theClassLoader = "classLoader";
-    private final static String theMediator = "pumaMediator";
-    private final static String theBodyConfig = "bodyConfigSpec";
-    private final static String theRegClient = "theRegistryClient";
-    private final static String commandEvent="commandEvent";
-    private final static String appContext="appContext";
+    private final static String REGKEY_ClassLoader = "classLoader";
+    private final static String REGKEY_Mediator = "pumaMediator";
+    private final static String REGKEY_BodyConfSpec = "bodyConfigSpec";
+    private final static String REGKEY_PumaRegCli = "theRegistryClient";
+    private final static String REGKEY_CommandEvent="commandEvent";
+    private final static String REGKEY_AppContext="appContext";
     
     private final static String[] theClassNameArray = {
         VWorldRegistry.class.getName()
@@ -37,57 +38,69 @@ public class VWorldMapperLifecycle extends BasicDebugger implements ServiceLifec
     private final static ServiceDependency[] theDependencyArray = {
 //        new ServiceDependency(theClassLoader, ClassLoader.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
 //        ServiceDependency.UpdateStrategy.STATIC, Collections.EMPTY_MAP),
-        new ServiceDependency(theMediator, PumaContextMediator.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
+		makeUnaryStaticServiceDep(REGKEY_Mediator, PumaContextMediator.class),
+		makeUnaryStaticServiceDep(REGKEY_BodyConfSpec, ArrayList.class),
+		makeUnaryStaticServiceDep(REGKEY_PumaRegCli, PumaRegistryClient.class),
+		makeUnaryStaticServiceDep(REGKEY_CommandEvent, CommandEvent.class),
+		makeUnaryStaticServiceDep(REGKEY_AppContext, PumaAppContext.class)
+
+		/*
+		 ABOVE 5 lines replaces the below 5 lines.   D.R.Y.
+		 * 
+        new ServiceDependency(REGKEY_Mediator, PumaContextMediator.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
         ServiceDependency.UpdateStrategy.STATIC, Collections.EMPTY_MAP),
-        new ServiceDependency(theBodyConfig, ArrayList.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
+        new ServiceDependency(REGKEY_BodyConfSpec, ArrayList.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
         ServiceDependency.UpdateStrategy.STATIC, Collections.EMPTY_MAP),
-        new ServiceDependency(theRegClient, PumaRegistryClient.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
+        new ServiceDependency(REGKEY_PumaRegCli, PumaRegistryClient.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
         ServiceDependency.UpdateStrategy.STATIC, Collections.EMPTY_MAP),
-        new ServiceDependency(commandEvent, CommandEvent.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
+        new ServiceDependency(REGKEY_CommandEvent, CommandEvent.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
         ServiceDependency.UpdateStrategy.STATIC, Collections.EMPTY_MAP),
-        new ServiceDependency(appContext, PumaAppContext.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
+        new ServiceDependency(REGKEY_AppContext, PumaAppContext.class.getName(), ServiceDependency.Cardinality.MANDATORY_UNARY,
         ServiceDependency.UpdateStrategy.STATIC, Collections.EMPTY_MAP)
+		*/
             
     };
+	
+	private static ServiceDependency makeUnaryStaticServiceDep(String regKey, Class depClazz) { 
+		return new ServiceDependency(regKey, depClazz.getName(),  ServiceDependency.Cardinality.MANDATORY_UNARY, 
+				ServiceDependency.UpdateStrategy.STATIC, Collections.EMPTY_MAP);
+	}
 
-    @Override
-    public VWorldRegistry createService(Map<String, Object> dependencyMap) {
+    @Override public VWorldRegistry createService(Map<String, Object> dependencyMap) {
         VWorldRegistry vworldreg = new VWorldRegistry();
-        vworldreg.setRegClient((PumaRegistryClient) dependencyMap.get(theRegClient));
+        vworldreg.setRegClient((PumaRegistryClient) dependencyMap.get(REGKEY_PumaRegCli));
         
         try {
-            vworldreg.initVWorldUnsafe((PumaContextMediator) dependencyMap.get(theMediator));
+            vworldreg.initVWorldUnsafe((PumaContextMediator) dependencyMap.get(REGKEY_Mediator));
         } catch (Throwable t) {
             getLogger().warn("%%%%%%%%%%%%%%%%%%%%%%% Error with VWorldMapper init %%%%%%%%%%%%%%%%%%%%%%%");
         }
-        
         PumaContextCommandBox pCCB=new PumaContextCommandBox(vworldreg);
-        pCCB.setAppContext((PumaAppContext)dependencyMap.get(appContext));
-        CommandEvent ce=(CommandEvent)dependencyMap.get(commandEvent);
+        pCCB.setAppContext((PumaAppContext)dependencyMap.get(REGKEY_AppContext));
+        CommandEvent ce=(CommandEvent)dependencyMap.get(REGKEY_CommandEvent);
         ce.setUpdater((Updater)pCCB);
         vworldreg.setContextCommandBox(pCCB);
         pCCB.reloadCommandSpace();
         //code for connecting bodies
-        ArrayList<BodyConfigSpec> bodyConfig = (ArrayList<BodyConfigSpec>) dependencyMap.get(theBodyConfig);
+        ArrayList<BodyConfigSpec> bodyConfig = (ArrayList<BodyConfigSpec>) dependencyMap.get(REGKEY_BodyConfSpec);
 
         for (BodyConfigSpec body : bodyConfig) {
             try {
-                System.out.println("STATUS!!!!!!!!!!: ");
-                if(body.getGraphIdentForBony()!=null)
-                {
-                    System.out.println("GraphIdentForBony is non-null: "+body.getGraphIdentForBony());
+                Ident		boneSrcGraphID = body.getBoneSrcGraphID();
+				RepoClient	repoCli = body.getRepoClient();
+				
+                if (boneSrcGraphID != null) {
+                    getLogger().debug("boneSrcGraphID is non-null {}", boneSrcGraphID);
                 }
-                if(body.getRepoClient()!=null)
-                {
-                    System.out.println("REPOCLIENT FOUND: "+(body.getRepoClient()).toString());
+                if (repoCli !=null) {
+                    getLogger().debug("REPOCLIENT FOUND: {}", repoCli);
                 }
-				FigureConfig humaFigCfg = body.getHumCfg();
+				FigureConfig humaFigCfg = body.getHumaFigureConfig();
+				
 				Ident charID = humaFigCfg.getFigureID();
-                getLogger().info("Initializing Virtual World Humanoid for charID={}", charID);
-				// This have been our bug, once combined with the idea of a 
-				// single vworldreg.myCharID:  vworldreg.setCharID(charID);
+
 				getLogger().info("Calling initVworldHumanoid for charID={}", charID);
-                vworldreg.initVWorldHumanoid(body.getRepoClient(), body.getGraphIdentForBony(), humaFigCfg);
+                vworldreg.initVWorldHumanoid(body.getRepoClient(), boneSrcGraphID, humaFigCfg);
 				getLogger().info("Calling connnectBonyRobotToHumanoidFigure for charID={}", charID);
 				vworldreg.connectBonyRobotToHumanoidFigure(body.getModelRobot(), charID);
             } catch (Throwable t) {
@@ -102,26 +115,20 @@ public class VWorldMapperLifecycle extends BasicDebugger implements ServiceLifec
         return vworldreg;
     }
 
-    @Override
-    public VWorldRegistry handleDependencyChange(VWorldRegistry client, String changeType, String dependencyName,
+    @Override  public VWorldRegistry handleDependencyChange(VWorldRegistry client, String changeType, String dependencyName,
             Object dependency, Map<String, Object> availableDependencies) {
         return null;
     }
 
-    @Override
-    public String[] getServiceClassNames() {
+    @Override public String[] getServiceClassNames() {
         return theClassNameArray;
     }
 
-    @Override
-    public void disposeService(VWorldRegistry t, Map<String, Object> map) {
-
+    @Override public void disposeService(VWorldRegistry t, Map<String, Object> map) {
         t = null;
-
     }
 
-    @Override
-    public List<ServiceDependency> getDependencySpecs() {
+    @Override public List<ServiceDependency> getDependencySpecs() {
         return Arrays.asList(theDependencyArray);
     }
 }
