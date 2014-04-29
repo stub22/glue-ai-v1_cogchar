f771fa416aacf419a79ab35538a9eac32b7735cd
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2014-04-29 23:56:32 +0000

    PUMA no longer depends on Lift.
    Promoted network config code into Cogchar.
    The o.f.b.netconfig bundle is going away.
    
    The "bootstrap.liftweb" package is now optional,
    so Lift bootclass can be overriden using web.xml.

---
+++ maven/org.cogchar.lib.core.impl/src/main/java/org/cogchar/impl/netconf/CogCharNetworkServiceLauncher.java
@@ -0,0 +1,45 @@
+/*
+ *  Copyright 2014 by The Friendularity Project (www.friendularity.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cogchar.impl.netconf;
+
+import org.cogchar.impl.netconf.CogCharNetworkConfigAmbassador;
+
+// Note these specific implementation imports.
+
+import org.jflux.impl.services.rk.lifecycle.ServiceLifecycleProvider;
+import org.jflux.impl.services.rk.lifecycle.utils.SimpleLifecycle;
+import org.jflux.impl.services.rk.osgi.lifecycle.OSGiComponent;
+import org.osgi.framework.BundleContext;
+
+import org.cogchar.bind.lift.LiftAmbassador;
+
+/**
+ * Pulled from the Activator of the old prototype: o.f.b.netconfig
+ * @author Stu B. <www.texpedient.com>
+ */
+
+public class CogCharNetworkServiceLauncher {
+	
+	
+	public static void startCogCharAmbassadors(BundleContext context) {
+		// Connect a CogCharNetworkConfigAmbassador as a (no lifecycle) managed service, so lifter can use it
+		ServiceLifecycleProvider ambassadorCycle =
+				new SimpleLifecycle(new CogCharNetworkConfigAmbassador(), LiftAmbassador.LiftNetworkConfigInterface.class);
+		OSGiComponent cncaComp = new OSGiComponent(context, ambassadorCycle);
+		cncaComp.start();
+	}	
+}
