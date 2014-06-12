55dccd8cafd6863a8312287b50e842c4dd21b520
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2014-06-12 21:37:10 +0000

    Adding explanatory READMEs, package-level comments, unit tests.

---
+++ maven/org.cogchar.lib.onto/src/main/java/org/cogchar/gen/oname/package-info.java
@@ -0,0 +1,11 @@
+/**
+ * Classes in this "oname" package is generated from the model resources in org.cogchar.onto.
+ * These package level comments are stored in a regular editable source directory,
+ * in the file package-info.java.  This file is seen by Javadoc, but not by the regular
+ * java compiler.
+ * 
+ * The first line (as determined by line-break?) is copied as a package-header comment.
+ */
+package org.cogchar.gen.oname;
+
+//rest of the file is empty
