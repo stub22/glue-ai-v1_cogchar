7de78f7ba88bac81884a3fb503e7212026d68d20
Ben Jenkins <benjenkinsv95@gmail.com>
2014-06-13 22:37:35 +0000

    Added code to allow the SkyBox to be used instead of a background color if a SkyBox image named "default.dds" is included inside of the Content projects textures/skybox/ folder. Richard requested the SkyBox.

--- maven/org.cogchar.lib.render.hominoid/src/main/java/org/cogchar/render/app/humanoid/HumanoidRenderWorldMapper.java
+++ maven/org.cogchar.lib.render.hominoid/src/main/java/org/cogchar/render/app/humanoid/HumanoidRenderWorldMapper.java
@@ -39,98 +39,125 @@ import org.cogchar.api.cinema.LightsCameraConfig;
 import org.cogchar.api.cinema.PathConfig;
 import org.cogchar.api.cinema.ThingAnimConfig;
 
+import com.jme3.scene.Node;
+import com.jme3.asset.AssetManager;
+import com.jme3.util.SkyFactory;
 
 /**
  *
  * @author Ryan Biggs
  */
 public class HumanoidRenderWorldMapper {
-	private static Logger theLogger = LoggerFactory.getLogger(HumanoidRenderWorldMapper.class);
 
-	
-	public void initLightsAndCamera(RepoClient qi, HumanoidRenderContext hrc, Ident qGraph) {
-		// Note that this requires the magical hrc power object, whereas we would prefer it didn't.
-		// Called from a stack like this:
-		// PumaVirtualWorldMapper.initCinematicStuff()
-		// PumaVirtualWorldMapper.initVirtualWorlds()
-		// PumaAppContext.initCinema()
-		// which may be called from 3 places:
-		// PumaBooter.pumaBootUnsafeUnderOSGi(), PumaAppContext.reloadAll(), PumaContextCommandBox.processUpdateRequestNow()
-		
-		// Builds a config object set from the supplied graph:
-		LightsCameraConfig lcc = new LightsCameraConfig(qi, qGraph);
-		RenderRegistryClient rendRegCli = hrc.getRenderRegistryClient();
-		CameraMgr cm = rendRegCli.getOpticCameraFacade(null);
-		cm.initCamerasFromConfig(lcc, rendRegCli);
-		LightFactory lf = rendRegCli.getOpticLightFacade(null);
-		lf.initLightsFromConfig(lcc, hrc);
-		setBackgroundColor(hrc, lcc);
-	}
+    private static Logger theLogger = LoggerFactory.getLogger(HumanoidRenderWorldMapper.class);
 
-	private PathMgr getPathMgr(HumanoidRenderContext hrc) {
-		return hrc.getGoodyRenderRegistryClient().getScenePathFacade(null);
-	}
-	
-	private SpatialAnimMgr getSpatialAnimMgr(HumanoidRenderContext hrc) {
-		return hrc.getGoodyRenderRegistryClient().getSceneAnimFacade(null);
-	}
-	
-	public void initWaypoints(RepoClient qi, Ident qGraph) {
-		AnimWaypointsConfig awc = new AnimWaypointsConfig(qi, qGraph);
-		AnimWaypointsConfig.setMainConfig(awc);
-	}
-	
-	public void initPaths(RepoClient qi, HumanoidRenderContext hrc, Ident qGraph) {
-		PathConfig pc = new PathConfig(qi, qGraph);
-		getPathMgr(hrc).storeAnimationsFromConfig(pc, hrc);
-	}
-	
-	public void initThingAnims(RepoClient qi, HumanoidRenderContext hrc, Ident qGraph) {
-		ThingAnimConfig tac = new ThingAnimConfig(qi, qGraph);
-		getSpatialAnimMgr(hrc).storeAnimationsFromConfig(tac, hrc);
-	}
-	
-	public void clearLights(HumanoidRenderContext hrc) {
-		RenderRegistryClient rendRegCli = hrc.getRenderRegistryClient();
-		LightFactory lf = rendRegCli.getOpticLightFacade(null);
-		lf.clearLights(hrc);
-	}
-	
-	public void clearCinematics(HumanoidRenderContext hrc) {
-		// Now clears Paths / Spatial Animations
-		getPathMgr(hrc).clearAnimations();
-		getSpatialAnimMgr(hrc).clearAnimations();
-	}
-	
-	public void clearViewPorts(HumanoidRenderContext hrc) {
-		RenderRegistryClient rrc = hrc.getRenderRegistryClient();
-		CameraMgr cm = rrc.getOpticCameraFacade(null);
-		cm.clearViewPorts(rrc);
-	}
-	
-	private void setBackgroundColor(HumanoidRenderContext hrc, LightsCameraConfig lcc) {
-		ColorRGBA bgColor = new ColorRGBA(lcc.backgroundColor[0], lcc.backgroundColor[1], lcc.backgroundColor[2], lcc.backgroundColor[3]);
-		WorkaroundAppStub stub = hrc.getAppStub();
-		ViewPort vp = stub.getPrimaryAppViewPort();
-		vp.setBackgroundColor(bgColor);
-	}
-	
-	
-	// A temporary way to make it possible to interact with figures... but ultimately, Humanoids aren't quite goodies!
-	public void addHumanoidGoodies(VWorldEntityActionConsumer consumer, HumanoidRenderContext hrc) {
-		GoodyRenderRegistryClient grrc = hrc.getGoodyRenderRegistryClient();
-		Map<Ident, HumanoidFigure> humanoidFigures = hrc.getHumanoidFigureManager().getHumanoidFigures();
-		for (Ident figureUri : humanoidFigures.keySet()) {
-			theLogger.info("Adding a HumanoidFigureGoodyWrapper for {}", figureUri);
-			HumanoidFigure figure = humanoidFigures.get(figureUri);
-			VWorldHumanoidFigureEntity vhfe = new VWorldHumanoidFigureEntity(grrc, figureUri, figure);
-			consumer.addGoody(vhfe);
-		}
-	}
+    public void initLightsAndCamera(RepoClient qi, HumanoidRenderContext hrc, Ident qGraph) {
+        // Note that this requires the magical hrc power object, whereas we would prefer it didn't.
+        // Called from a stack like this:
+        // PumaVirtualWorldMapper.initCinematicStuff()
+        // PumaVirtualWorldMapper.initVirtualWorlds()
+        // PumaAppContext.initCinema()
+        // which may be called from 3 places:
+        // PumaBooter.pumaBootUnsafeUnderOSGi(), PumaAppContext.reloadAll(), PumaContextCommandBox.processUpdateRequestNow()
 
-	/* No longer needed unless we want to init from Turtle again
-	public LightsCameraConfig readLightsCameraConfig(String rdfConfigFlexPath, ClassLoader optResourceClassLoader) {
-		return AssemblerUtils.readOneConfigObjFromPath(LightsCameraConfig.class, rdfConfigFlexPath, optResourceClassLoader);
-	}
-	*/
+        // Builds a config object set from the supplied graph:
+        LightsCameraConfig lcc = new LightsCameraConfig(qi, qGraph);
+        RenderRegistryClient rendRegCli = hrc.getRenderRegistryClient();
+        CameraMgr cm = rendRegCli.getOpticCameraFacade(null);
+        cm.initCamerasFromConfig(lcc, rendRegCli);
+        LightFactory lf = rendRegCli.getOpticLightFacade(null);
+        lf.initLightsFromConfig(lcc, hrc);
+
+        setBackgroundColor(hrc, lcc);
+        /*
+         * Overrides the background color if a skybox image is available at
+         * Content/textures/skybox/default.dds
+         * /
+        setSkyBox(hrc);
+
+    }
+
+    private PathMgr getPathMgr(HumanoidRenderContext hrc) {
+        return hrc.getGoodyRenderRegistryClient().getScenePathFacade(null);
+    }
+
+    private SpatialAnimMgr getSpatialAnimMgr(HumanoidRenderContext hrc) {
+        return hrc.getGoodyRenderRegistryClient().getSceneAnimFacade(null);
+    }
+
+    public void initWaypoints(RepoClient qi, Ident qGraph) {
+        AnimWaypointsConfig awc = new AnimWaypointsConfig(qi, qGraph);
+        AnimWaypointsConfig.setMainConfig(awc);
+    }
+
+    public void initPaths(RepoClient qi, HumanoidRenderContext hrc, Ident qGraph) {
+        PathConfig pc = new PathConfig(qi, qGraph);
+        getPathMgr(hrc).storeAnimationsFromConfig(pc, hrc);
+    }
+
+    public void initThingAnims(RepoClient qi, HumanoidRenderContext hrc, Ident qGraph) {
+        ThingAnimConfig tac = new ThingAnimConfig(qi, qGraph);
+        getSpatialAnimMgr(hrc).storeAnimationsFromConfig(tac, hrc);
+    }
+
+    public void clearLights(HumanoidRenderContext hrc) {
+        RenderRegistryClient rendRegCli = hrc.getRenderRegistryClient();
+        LightFactory lf = rendRegCli.getOpticLightFacade(null);
+        lf.clearLights(hrc);
+    }
+
+    public void clearCinematics(HumanoidRenderContext hrc) {
+        // Now clears Paths / Spatial Animations
+        getPathMgr(hrc).clearAnimations();
+        getSpatialAnimMgr(hrc).clearAnimations();
+    }
+
+    public void clearViewPorts(HumanoidRenderContext hrc) {
+        RenderRegistryClient rrc = hrc.getRenderRegistryClient();
+        CameraMgr cm = rrc.getOpticCameraFacade(null);
+        cm.clearViewPorts(rrc);
+    }
+
+    private void setBackgroundColor(HumanoidRenderContext hrc, LightsCameraConfig lcc) {
+        ColorRGBA bgColor = new ColorRGBA(lcc.backgroundColor[0], lcc.backgroundColor[1], lcc.backgroundColor[2], lcc.backgroundColor[3]);
+        WorkaroundAppStub stub = hrc.getAppStub();
+        ViewPort vp = stub.getPrimaryAppViewPort();
+        vp.setBackgroundColor(bgColor);
+    }
+
+    /**
+     * Ben wrote this method, at Richard's request. This will load a skybox
+     * background instead of a static color background. Asset is found in the
+     * content project, textures/skybox/
+     *
+     * @param hrc
+     */
+    private void setSkyBox(HumanoidRenderContext hrc) {
+        try {
+            Node root = hrc.getGoodyRenderRegistryClient().getJme3RootDeepNode(null);
+            AssetManager assetManager = hrc.getGoodyRenderRegistryClient().getJme3AssetManager(null);
+            root.attachChild(SkyFactory.createSky(
+                    assetManager, "textures/skybox/default.dds", false));
+        } catch (Exception e) {
+            theLogger.error("Skybox background image failed to load:", e);
+        }
+    }
+
+    // A temporary way to make it possible to interact with figures... but ultimately, Humanoids aren't quite goodies!
+    public void addHumanoidGoodies(VWorldEntityActionConsumer consumer, HumanoidRenderContext hrc) {
+        GoodyRenderRegistryClient grrc = hrc.getGoodyRenderRegistryClient();
+        Map<Ident, HumanoidFigure> humanoidFigures = hrc.getHumanoidFigureManager().getHumanoidFigures();
+        for (Ident figureUri : humanoidFigures.keySet()) {
+            theLogger.info("Adding a HumanoidFigureGoodyWrapper for {}", figureUri);
+            HumanoidFigure figure = humanoidFigures.get(figureUri);
+            VWorldHumanoidFigureEntity vhfe = new VWorldHumanoidFigureEntity(grrc, figureUri, figure);
+            consumer.addGoody(vhfe);
+        }
+    }
+
+    /* No longer needed unless we want to init from Turtle again
+     public LightsCameraConfig readLightsCameraConfig(String rdfConfigFlexPath, ClassLoader optResourceClassLoader) {
+     return AssemblerUtils.readOneConfigObjFromPath(LightsCameraConfig.class, rdfConfigFlexPath, optResourceClassLoader);
+     }
+     */
 }
