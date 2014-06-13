75f1fc994e18fb761670cf008e7f0f93ddd8dd31
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2014-06-13 04:09:44 +0000

    Testing variations of ontology load, using current Jena schemagen and RIOT API.

--- src_scala_core/org/cogchar/scalatest/OntoTest.scala
+++ src_scala_core/org/cogchar/scalatest/OntoTest.scala
@@ -30,5 +30,32 @@ object OntoTest {
 		println ("WebTier_owl2.SENDER.localName: " + WebTier_owl2.SENDER.getLocalName())
 		
 		println ("ID from ontoName: " + WebUserActionNames.idFromOntoName)
+		
+		// Let's load some of our favorite Cogchar ontologies from the runtime classpath,
+		// using the modern Jena file-finding API (which supercedes old "FileManager"),
+		// and then query them using some of the constants from our Schemagen .java files.
+		// 
+		//    https://jena.apache.org/documentation/io/rdf-input.html
+		//    https://jena.apache.org/documentation/javadoc/arq/org/apache/jena/riot/RDFDataMgr.html
+		// 
+		import org.apache.jena.riot.RDFDataMgr;
+		// Since we are in a main() unit test, we can ignore any complexity of bundle-classpath.
+		
+		val mcPath = "org/cogchar/onto/WebTier_owl2.ttl"
+		val m = RDFDataMgr.loadModel(mcPath) ;
+		println("Read model from classpath at [" + mcPath + "] = " + m)
+		
+		// When we use the "include-source" option during schemagen build step of o.c.lib.onto, 
+		// the oname classes contain their own embedded copy of their own source models, which is loaded
+		// by the classloader.  That is good and bad.   It's pretty cool if it always works perfectly,
+		// and doesn't consume too many resources.  
+		// 
+		// Currently we have include-source turned on for all our
+	}
+	
+	def testOntoLookupUnderOSGi() { 
+		// Before lastest StreamManager in Jena 2.10, we used some clumsy classloader-plugin approaches.
+		// But now, in the age of awesome, we can ...
+		// 
 	}
 }
