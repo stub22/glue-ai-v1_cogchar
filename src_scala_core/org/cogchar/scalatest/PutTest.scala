beeddf191b1a7cb66fd41d5000f6023f8b3c14d2
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2015-01-20 23:28:36 +0000

    Some experimental code combining MDir metadata and Scala actor concurrency.

--- src_scala_core/org/cogchar/scalatest/PutTest.scala
+++ src_scala_core/org/cogchar/scalatest/PutTest.scala
@@ -72,17 +72,121 @@ case class PTMsg(txt : String)
 
 case class StopMsg()
 
+case class Msg_CopyGraph
+
+import org.cogchar.api.owrap.mdir._
+import rdf2go.model.Model
+trait UriWrap {
+	def getR2GoURI : rdf2go.model.node.URI
+	def getIdent : Ident
+	def getJenaRes : jena.rdf.model.Resource
+}
+abstract class UriWrap_R2Go(r2goUri : rdf2go.model.node.URI) extends UriWrap {
+	override def getR2GoURI : rdf2go.model.node.URI = r2goUri
+}
+object UWrapFactory {
+	def makeUriWrap(r2goUri : rdf2go.model.node.URI) : UriWrap = ???
+	def makeUriWrap(mthing : MThing) : UriWrap = makeUriWrap(mthing.asURI)
+}
+trait IndivWrap[IType <: MThing] {
+	def getJavaClazz : Class[_ <: IType] 
+
+	def getIndivUriWrap : UriWrap 
+	
+	def getClazzUriWrap : UriWrap = ???
+	
+	def getIndivInR2GoModel(mR2go : rdf2go.model.Model, writeTypeIfNeeded : Boolean) : IType = ???
+	
+	// Thus we can promote any IndivWrap to be a bound IndivWrap.
+	def makeBoundIW(mR2go : rdf2go.model.Model, writeType : Boolean) : BoundIW[IType] = {
+		val mthing : MThing = new MThing(mR2go, getIndivUriWrap.getR2GoURI, writeType)
+		val strongIndiv : IType = mthing.castTo(getJavaClazz).asInstanceOf[IType]
+		new DirectBoundIW(strongIndiv)
+	}
+	// rdf2go.model.Model
+	// def getResInJenaModel(mJena : jena.rdf.model.Model) : 
+}
+trait BoundIW[IType <: MThing] extends IndivWrap[IType] {
+	// The returned indiv should be connected to a readable open R2Go model (possibly in a TDB read trans).
+	def getIndiv : IType = ???  
+	
+	def getBoundR2GoModel : rdf2go.model.Model = getIndiv.getModel
+	
+	override def makeBoundIW(mR2go : rdf2go.model.Model, writeType : Boolean) : BoundIW[IType] = {
+		// If self already bound to that model, then self.
+		super.makeBoundIW(mR2go, writeType)
+	}
+}
+
+// We presume that myDirectIndiv is bound to some readable open M2Go model (possibly in a TDB read trans).
+case class DirectBoundIW [IType <: MThing](private val myDirectIndiv : IType) extends BoundIW[IType] {
+	override def getIndiv : IType = myDirectIndiv
+	override def getJavaClazz : Class[_ <: IType] = myDirectIndiv.getClass
+	private lazy val myIndivUriWrap = UWrapFactory.makeUriWrap(myDirectIndiv)
+	override def getIndivUriWrap : UriWrap = myIndivUriWrap
+	private lazy val myClazzUriWrap : UriWrap =  UWrapFactory.makeUriWrap(myDirectIndiv.getRDFSClassURI)
+}
+// Consider supplying classUri as additional param
+case class OneIndivWrap[IType <: MThing](indivUriWrap : UriWrap, jClz : Class[_ <: IType]) extends IndivWrap[IType] {
+	override def getIndivUriWrap : UriWrap  = indivUriWrap
+	override def getJavaClazz : Class[_ <: IType] = jClz
+}
+object IWrapFactory {
+	def makeIWrap[IType <: MThing](indiv : IType) : IndivWrap[IType] = ???
+	def makeIWrap[IType <: MThing](indivUriWrap : UriWrap, clz : Class[IType]) : IndivWrap[IType] = ???
+	
+}
+
+
+
+// We generally have a need for typed tuples containing RDF2Go instances in known states of resolution+backing.
+abstract class GraphHostWrap[GHType <: GraphHost](rslvdJClz : Class[GHType]) {
+	def getGraphHost : GHType 
+}
+
+trait GraphOpWrap[GOType <: GraphOp] {
+	def getGraphOp : GOType
+}
+trait GPointerSetWrap { 
+	def getGPointerSet : GraphPointerSet
+}
+trait AnyPointerWrap[APType <: Pointer] {
+	def getPointer : APType
+}
+
+trait GraphPointerWrap extends AnyPointerWrap[GraphPointer] {
+	// Onto has sub-variants:  GP_Open, GP_Release, GP_Snapshot - which from java perspective are more like immutable state than a type.
+	def getGraphPointer : GraphPointer = getPointer
+	def makeOpen : GraphPointerWrap
+	def makeRelease : GraphPointerWrap
+	def makeSnapshot : GraphPointerWrap
+}
+
+trait CPathEntityWrap[CEType <: ClasspathEntity]
+
+trait MvnArtWrap[MAType <: MavenArtifact]
+
+case class FunnyTestAct(op : GraphOpWrap[GraphOp])
+
+// Idea is that we can build up an executable test plan using queries on a set of test data acts, which may itself
+// be partly generated through inference rules.
+case class FunnyTestStep(myPrevSteps : Set[FunnyTestStep], myAct : FunnyTestAct)
+ 
+
+// case class
+
 // Here is the "old" Scala API summary, which we still rely on as of 2015-Jan.
 // http://docs.scala-lang.org/overviews/core/actors.html
 // We are trying to limit the fanciness of our Actors code, so that it will work with minimal changes under Scala 2.11.
 class WritingActor  extends scala.actors.Actor with VarargsLogging {
+	// The lifecycle of the actor 
 	override  def act : Unit = {
 		var myDoneFlag : Boolean = false
 
 		// self is a method on the companion object
 		def getSelf = scala.actors.Actor.self
 		// val selfThing = "[self undefined in legacy Actors]"
-		// Loop and process messages until we
+		// Loop and process messages until the doneMarker is set.
 		while (!myDoneFlag) {
 			info2("WritingActor at top of loop for {} which is {}", getSelf, this)
 			// Blocking "receive" call uses scala actors runtime to wait for a message for this actor, which is
@@ -100,7 +204,8 @@ class WritingActor  extends scala.actors.Actor with VarargsLogging {
 				}
 				// Best practice in production actors code is usually *not* to handle the wildcard case, so that
 				// frameworks (e.g. Akka EventBus) can do something with the 'unhandled'.  However we are currently 
-				// handling it here, naively.
+				// handling wildcard here, naively.  Interestingly, "other" shows up as a wrapped object
+				// scala.actors.Reactor$$anon$3@6156ee8e
 				case other => {
 					info1("WritingActor got other message: {}, absorbing it (rather than leaving 'unhandled')", other)
 				}
