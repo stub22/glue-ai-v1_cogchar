4405ea3c1f3f1d4542785e35df3f3e2b71cd941c
stub22 <stub22@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2015-03-07 02:49:50 +0000

    Moved EntryHostFinder.scala into its own file.

---
+++ trunk/src_scala_core/org/cogchar/blob/entry/EntryHostFinder.scala
@@ -0,0 +1,41 @@
+/*
+ *  Copyright 2015 by The Cogchar Project (www.cogchar.org).
+ * 
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package org.cogchar.blob.entry
+
+/**
+
+ */
+
+// These can be provided from main(), or an injected service (OSGi/JFlux).
+// Knows about some ordered sequence of hosts to query.
+// 
+// This finder may have several impls, but all of them should be independent of the sub-types of EntryHost.
+// (So we could access any kind of EntryHost from any kind of EntryHostFinder).
+
+trait EntryHostFinder {
+	// Generally these were registered by some impl-specific means.
+	def findEntryHostsInOrder : Seq[EntryHost]
+	
+	def findFirstMatchingPlainEntry(locUri : java.net.URI) : Option[PlainEntry]
+	def findFirstMatchingFolderEntry(locUri : java.net.URI) : Option[FolderEntry]
+	
+	def findAllMatchingPlainEntries(locUri : java.net.URI) : Set[FolderEntry]
+	def findAllMatchingFolderEntries(locUri : java.net.URI) : Set[FolderEntry]
+	
+	def searchAllPlainEntriesBySuffix(locUri : java.net.URI, suffixes : Set[String], deepSearch : Boolean, maxResultCount : Int):  Set[PlainEntry]
+}
+
