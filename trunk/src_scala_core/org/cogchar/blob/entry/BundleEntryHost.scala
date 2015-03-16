319fe560fe21b3378014ba99988f04df80090623
benjenkinsv95@gmail.com <benjenkinsv95@gmail.com@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2015-03-16 20:36:15 +0000

    I think these should work for looking at resources in an OSGI bundle but need to test them more.

--- trunk/src_scala_core/org/cogchar/blob/entry/BundleEntryHost.scala
+++ trunk/src_scala_core/org/cogchar/blob/entry/BundleEntryHost.scala
@@ -18,6 +18,30 @@ package org.cogchar.blob.entry
 
 // *******************************************************************
 // Second important case is when we want to scan through all files in a folder in some OSGi bundle.
+/**
+ * I am choosing to keep track of URLs because they will allow us to get the size of the files and last modified time of the files
+ * Using java.nio.file.Path objects would require us to create quite a few FileSystem objects for each jar used in the felix cache.
+ * Using java.io.File objects would fail because you cannot create a File object for a path inside of a jar.
+ * You can get a java.io.File object by using bundle.getDataFile(path) but it doesn't have size or modified info for some reason...
+ * Below is an example of how to get the size and last modified time out of a URL.
+ * The last modified time will be the same for every item in the bundle, as the bundle was generated which counts as modifying a file...
+ *          String filePath; // An actual file path
+ *          URL fileURL = bundle.getEntry(filePath);
+ *          long size = -1;
+ *          long lastModifiedTime = -1;
+ *          URLConnection conn = null;
+ *          try {
+ *              conn =  fileURL.openConnection(); 
+ *              size = conn.getContentLength();
+ *              // Do not use conn.getLastModifiedLong() the URLConnection type that is returned is specifically implemented
+ *              // By apache and they did not properly implement the long version of this method... 
+ *              lastModifiedTime = conn.getLastModified(); 
+ *          } catch (IOException e) {
+ *              System.out.println("Failed to open url");
+ *          } 
+ */
+
+import java.net.URL
 
 class BundleEntryHost(bundle : org.osgi.framework.Bundle) extends EntryHost { 
 	// Using Bundle's nice method: findEntries(java.lang.String path, java.lang.String filePattern, boolean recurse) 
@@ -28,22 +52,71 @@ class BundleEntryHost(bundle : org.osgi.framework.Bundle) extends EntryHost {
 		import scala.collection.JavaConverters._
 		urlEnum.asScala.toSet
 	}
-	override def findFolderEntry(path : String) : Option[FolderEntry] = ???
-	override def findPlainEntry(path : String) : Option[PlainEntry] = ???
+        
+	override def findFolderEntry(path : String) : Option[FolderEntry] = {
+		val resolvedURL_opt : Option[java.net.URL] = resolvePathToURL(path) 
+		resolvedURL_opt.map(url => new BundleFolderEntry(bundle, url.toURI))
+
+	}
+	override def findPlainEntry(path : String) : Option[PlainEntry] = {
+		val resolvedURL_opt : Option[java.net.URL] = resolvePathToURL(path) 
+		resolvedURL_opt.map(url => new BundlePlainEntry(bundle, url.toURI))
+	}
+        
+    private def resolvePathToURL(path: String) : Option[java.net.URL] = {
+		val pathWithLeadingSlash : String = if (path.startsWith("/")) path else "/" + path
+		val resolvedURL_opt = Option(bundle.getResource(pathWithLeadingSlash));
+		if (resolvedURL_opt.isEmpty) {
+			warn1("Could not resolve resource path: {}", pathWithLeadingSlash)
+		}
+		resolvedURL_opt
+	}
 }
-class BundleEntry(locUri : java.net.URI) extends Entry {
+class BundleEntry(bundle : org.osgi.framework.Bundle, locUri : java.net.URI) extends Entry {
 	override def getJavaURI : java.net.URI = locUri
 }
-class BundleFolderEntry(host : BundleEntryHost, locUri : java.net.URI) extends BundleEntry (locUri) with FolderEntry {
-	def findDirectPlainEntries: Traversable[PlainEntry] = ???
-	def findDirectSubFolders: Traversable[FolderEntry] = ???	
-	// BundleFolderEntry could supply its own search impls based on the getMatchingEntryURLs method above
+class BundleFolderEntry(bundle : org.osgi.framework.Bundle, locUri : java.net.URI) extends BundleEntry (bundle, locUri) with FolderEntry {
+        // BundleFolderEntry could supply its own search impls based on the getMatchingEntryURLs method above
 	// 
 	// Note these 2 different function-arg type syntaxes are equivalent
 	// override def searchDirectPlainEntries(filt: PlainEntry => Boolean): Set[PlainEntry] = ??? // recurse = false
 	// override def searchDeepPlainEntries(filt: Function1[PlainEntry,Boolean]): Set[PlainEntry] = ???  // recurse = true
+    
+    
+	lazy val myBundleDelegate: Option[OSGIFolderEntry] = {
+
+        var path_opt: Option[URL] = None
+        try {
+            import scala.collection.JavaConversions._
+            debug1("Making Java OSGI Bundle URL for locUri: {}", locUri)
+            val path = locUri.toURL
+            path_opt = Option(path)
+                
+        } catch {
+            case t: Throwable => error2("Error instantiating java.nio.file.Path for uri: {}, exc: {}", locUri, t)
+        }
+        path_opt.map(new OSGIFolderEntry(bundle, _))
+    }
+	
+	
+	override def findDirectPlainEntries: Traversable[PlainEntry] = {
+		if (myBundleDelegate.isDefined) {
+	
+			myBundleDelegate.get.findDirectPlainEntries  
+		} else {
+			Set()
+		}
+	}
+	override def findDirectSubFolders: Traversable[FolderEntry] = {
+		if (myBundleDelegate.isDefined) {
+			myBundleDelegate.get.findDirectSubFolders
+		} else {
+			Set()
+		}
+	}	
+	
 	
 	
 }
-class BundlePlainEntry(locUri : java.net.URI) extends BundleEntry(locUri) with PlainEntry {
+class BundlePlainEntry(bundle : org.osgi.framework.Bundle, locUri : java.net.URI) extends BundleEntry(bundle, locUri) with PlainEntry {
 }
