ba67d460616efc75c094533bc718be3c6b97dace
benjenkinsv95@gmail.com <benjenkinsv95@gmail.com@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2015-03-25 17:08:33 +0000

    Updating the test to be able to scan org.cogchar.bundle.core for contents instead.

--- trunk/maven/org.cogchar.bundle.app.new.puma/src/main/java/org/cogchar/bundle/app/puma/BundleScannerTest.java
+++ trunk/maven/org.cogchar.bundle.app.new.puma/src/main/java/org/cogchar/bundle/app/puma/BundleScannerTest.java
@@ -2,16 +2,37 @@ package org.cogchar.bundle.app.puma;
 
 import org.cogchar.blob.circus.GraphScanTest;
 import org.cogchar.blob.entry.BundleEntryHost;
+import org.cogchar.bundle.core.CogcharCoreActivator;
 import org.ontoware.rdf2go.model.Model;
 import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+import org.osgi.framework.FrameworkUtil;
 
 public class BundleScannerTest {
 
-    public static void scanBundle(BundleContext context) throws Exception {
+    public static void scanBundleFromContext(BundleContext context) throws Exception {
         // TODO add activation code here
 
         Bundle bundle = context.getBundle();
+        scanBundleContents(bundle);
+        System.exit(0);
+    }
+
+    public static void scanBundleCore() throws Exception {
+        final Class<CogcharCoreActivator> bundleClass = org.cogchar.bundle.core.CogcharCoreActivator.class;
+        scanBundleFromClass(bundleClass);
+    }
+
+    public static void scanBundleFromClass(Class bundleClass) throws Exception {
+
+        // TODO add activation code here
+        Bundle bundle = FrameworkUtil.getBundle(bundleClass);
+        scanBundleContents(bundle);
+
+        System.exit(0);
+    }
+
+    private static void scanBundleContents(Bundle bundle) {
         BundleEntryHost testBundleEntryHost = new BundleEntryHost(bundle);
         Model bundleScanResultModel = GraphScanTest.makeEmptyTempR2GoModel();
 
@@ -21,7 +42,5 @@ public class BundleScannerTest {
         System.out.printf("Deep-scanned OSGI Bundles \"%s\" folder and found %d results: %s\n", scannableFolderPath, foundRDFCount,
                 bundleScanResultModel.getUnderlyingModelImplementation().toString());
         System.out.println(foundRDFCount);
-
-        System.exit(0);
     }
 }
