6509a7bbf9b82711d0e64ee77455a3ed575cba49
Stu Btwotwo <1082227+stub22@users.noreply.github.com>
2015-04-07 19:43:17 +0000

    Refined implementation of type-promotion utility method.

--- src_scala_core/org/cogchar/blob/ghost/RRUtil.scala
+++ src_scala_core/org/cogchar/blob/ghost/RRUtil.scala
@@ -32,13 +32,20 @@ import rdfreactor.schema.rdfs.{Class => RDFR_Class}
 object RRUtil {
 	// promote does *not* check for any asserted rdf:type, and does *not* assert the rdf:type.
 	// Thus this type change is purely within Java space.
-	def promote[X](orig : ReactorRuntimeEntity, tgtClz : java.lang.Class[X]) : X = {
-		// If no special converter is registered for this type, castTo will wind up calling
-		// RDFReactorRuntime.resource2reactorbase, which in turn does reflection to find constructor, and then calls:
-		// return constructor.newInstance(new Object[] { model, node, false });
-		orig.castTo(tgtClz).asInstanceOf[X]
+	def promote[X <: ReactorRuntimeEntity](orig : ReactorRuntimeEntity, tgtClz : java.lang.Class[X]) : X = {
+		// Check using java runtime whether orig is already of the correct java type.
+		if(tgtClz.isInstance(orig)) {
+			// Since we are already the correct type, we short-circuit to just a type-cast for the existing instance.
+			orig.asInstanceOf[X]
+		} else {
+			// We need to produce a new java instance of the correct type.
+			// If no special converter is registered for this type, castTo will wind up calling
+			// RDFReactorRuntime.resource2reactorbase, which in turn does reflection to find constructor, and then calls:
+			// return constructor.newInstance(new Object[] { model, node, false });
+			orig.castTo(tgtClz).asInstanceOf[X]
+		}
 	}
-	def maybePromote[X](orig : ReactorRuntimeEntity, classURI : R2GoURI, tgtClz : java.lang.Class[X]) : Option[X] = {
+	def maybePromote[X <: ReactorRuntimeEntity](orig : ReactorRuntimeEntity, classURI : R2GoURI, tgtClz : java.lang.Class[X]) : Option[X] = {
 		// isInstanceof does (only) a check for rdf:type in the model.  
 		// model.contains(rdfResource, RDF.type, classURI);
 		// So if we want inheritance, must enable inference on the model and apply appropriate ontology.
