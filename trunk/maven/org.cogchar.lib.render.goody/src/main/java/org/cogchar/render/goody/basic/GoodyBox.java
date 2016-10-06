3891215b26e6e5d31db384925d7e56d38322bf22
benjenkinsv95@gmail.com <benjenkinsv95@gmail.com@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2016-10-06 20:59:16 +0000

    GoodyBox will now keep track of its scale.

--- trunk/maven/org.cogchar.lib.render.goody/src/main/java/org/cogchar/render/goody/basic/GoodyBox.java
+++ trunk/maven/org.cogchar.lib.render.goody/src/main/java/org/cogchar/render/goody/basic/GoodyBox.java
@@ -1,12 +1,12 @@
 /*
  *  Copyright 2013 by The Cogchar Project (www.cogchar.org).
- * 
+ *
  *  Licensed under the Apache License, Version 2.0 (the "License");
  *  you may not use this file except in compliance with the License.
  *  You may obtain a copy of the License at
- * 
+ *
  *       http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  *  Unless required by applicable law or agreed to in writing, software
  *  distributed under the License is distributed on an "AS IS" BASIS,
  *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,29 +28,29 @@ import org.cogchar.render.sys.goody.GoodyRenderRegistryClient;
  *
  * @author Ryan Biggs
  */
-
-
 public class GoodyBox extends BasicGoodyEntity {
-	
-	private final ColorRGBA DEFAULT_COLOR = ColorRGBA.LightGray;
 
-	public GoodyBox(BasicGoodyCtx bgc,  Ident boxUri, Vector3f position, Quaternion rotation,
-			ColorRGBA color, Vector3f size/*boolean rigidBodyPhysFlag*/) {
-		super(bgc, boxUri);
-		// This check is shared with BitCube and BitBox and should be factored out:
-		if (size == null) {
-			getLogger().warn("No size specified for GoodyBox, defaulting to size = 1");
-			size = new Vector3f(1.0f, 1.0f, 1.0f);
-		} else {
-			if (Math.abs(size.length() - 0.0f) < 0.001f) {
-				getLogger().warn("GoodyBox being created with zero size!");
-			}
-		} 
-		setPosition(position, QueueingStyle.QUEUE_AND_RETURN);
-		Mesh goodyBox = new Box(size.getX(), size.getY(), size.getZ());
-		if (color == null) {
-			color = DEFAULT_COLOR;
-		}
-		addGeometry(goodyBox, color, rotation);
-	}
+    private final ColorRGBA DEFAULT_COLOR = ColorRGBA.LightGray;
+
+    public GoodyBox(BasicGoodyCtx bgc, Ident boxUri, Vector3f position, Quaternion rotation,
+                    ColorRGBA color, final Vector3f newScale) {
+        super(bgc, boxUri);
+        // This check is shared with BitCube and BitBox and should be factored out:
+        if (newScale == null) {
+            getLogger().warn("No size specified for GoodyBox, defaulting to size = 1");
+        } else if (Math.abs(newScale.length() - 0.0f) < 0.001f) {
+            getLogger().warn("GoodyBox being created with zero size!");
+        } else {
+            setVectorScale(newScale, QueueingStyle.QUEUE_AND_RETURN);
+        }
+        setPosition(position, QueueingStyle.QUEUE_AND_RETURN);
+        /**
+         * The size is 1, but the scale will change its size.
+         */
+        Mesh goodyBox = new Box(1, 1, 1);
+        if (color == null) {
+            color = DEFAULT_COLOR;
+        }
+        addGeometry(goodyBox, color, rotation);
+    }
 }
