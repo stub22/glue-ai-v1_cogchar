90ee1c8873301e0090035f9872d8f8b5c02feeb9
benjenkinsv95@gmail.com <benjenkinsv95@gmail.com@fe788db4-5ddf-42ac-97bb-dcd4c2f7d622>
2017-01-30 20:59:57 +0000

    Moving loggers that use \{\d+\} syntax to slf4j

--- trunk/maven/org.cogchar.lib.core.impl/src/main/java/org/cogchar/impl/netconf/CogCharNetworkConfigAmbassador.java
+++ trunk/maven/org.cogchar.lib.core.impl/src/main/java/org/cogchar/impl/netconf/CogCharNetworkConfigAmbassador.java
@@ -16,28 +16,27 @@
 package org.cogchar.impl.netconf;
 
 import org.cogchar.impl.web.config.WebappNetworkConfigHandle;
-import java.util.logging.Level;
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 // import org.friendularity.bundle.netconfig.LinuxNetworkConfigurator;
 
 
 /**
- *
  * @author Ryan Biggs <rbiggs@skyriversoftware.com>
  */
 public class CogCharNetworkConfigAmbassador implements WebappNetworkConfigHandle {
-	
-	private static Logger theLogger = Logger.getLogger(CogCharNetworkConfigAmbassador.class.getName());
-	
+
+	private static final Logger theLogger = LoggerFactory.getLogger(CogCharNetworkConfigAmbassador.class);
+
 	@Override
 	public void configure(String ssid, String security, String key) {
 		try {
-			theLogger.log(Level.INFO, "Configuring network with SSID: {0} and security type: {1}", new Object[]{ssid, security});
+			theLogger.info("Configuring network with SSID: {} and security type: {}", ssid, security);
 			WiFiSecurity securityType = WiFiSecurity.valueOf(security);
 			NetworkConfigurator configurator = new LinuxNetworkConfigurator();
 			configurator.configureNetwork(new NetworkConfig(ssid, securityType, key));
 		} catch (Exception e) {
-			theLogger.log(Level.WARNING, "Could not match provided input \"{0}\" with a valid security type", security);
+			theLogger.warn("Could not match provided input \"{}\" with a valid security type", security);
 		}
 	}
 
